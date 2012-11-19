02671441d61f62dff1238e5dc06f9cd90c0514a1
Robert J. Macomber <robert.macomber@socrata.com>
2012-11-18 18:27:50 -0800

    Light optimization that makes almost 20ms (7-10%) difference

--- perftest/src/main/scala/com/socrata/datacoordinator/loader/loaderperf/PerfValue.scala
+++ perftest/src/main/scala/com/socrata/datacoordinator/loader/loaderperf/PerfValue.scala
@@ -10,7 +10,13 @@ case class PVId(value: Long) extends PerfValue {
 }
 case class PVNumber(value: BigDecimal) extends PerfValue {
   def sqlize = value.toString
-  override val hashCode = ScalaRunTime._hashCode(this)
+
+  @volatile private var hashComputed = false
+  private var hash: Int = _
+  override def hashCode = {
+    if(!hashComputed) { hash = ScalaRunTime._hashCode(this); hashComputed = true }
+    hash
+  }
 }
 case class PVText(value: String) extends PerfValue {
   def sqlize = "'" + value.replaceAllLiterally("'", "''") + "'"
