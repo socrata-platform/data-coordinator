dc2c6851f6b02002e3fd37ee9f9b2ccd6b2af203
Robert J. Macomber <robert.macomber@socrata.com>
2012-12-06 16:25:33 -0800

    Doc updates

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/metadata/DatasetMapLifecycleUpdater.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/metadata/DatasetMapLifecycleUpdater.scala
@@ -2,20 +2,26 @@ package com.socrata.datacoordinator.truth.metadata
 
 trait DatasetMapLifecycleUpdater {
   /** Creates a new table in the truthstore.
-    * @note Does not actually create any tables; this just updates the bookkeeping. */
+    * @note Does not actually create any tables; this just updates the bookkeeping.
+    * @note `datasetId` needs to be globally unique; if you have namespacing do it yourself.
+    * @throws Exception if `datasetId` is already in use. TODO: better exception.
+    * @return A [[com.socrata.datacoordinator.truth.metadata.VersionInfo]] that refers to an unpublished version. */
   def create(datasetId: String, tableBase: String): VersionInfo
 
   /** Completely removes a table (all its versions) from the truthstore.
-    * @note Does not actually drop (or queue for dropping) any tables; this just updates the bookkeeping. */
+    * @note Does not actually drop (or queue for dropping) any tables; this just updates the bookkeeping.
+    * @return `true` if `tableInfo` referred to a table that existed, otherwise `false`. */
   def delete(tableInfo: TableInfo): Boolean
 
   /** Delete this version of the table.
     * @note Does not drop the actual tables or even queue them for dropping; this just updates the bookkeeping.
-    * @throws IllegalArgumentException if the version does not name a snapshot or unpublished copy */
+    * @throws IllegalArgumentException if the version does not name a snapshot or unpublished copy.
+    * @return `true` if `versionInfo` referred to a version that existed, otherwise false. */
   def dropCopy(versionInfo: VersionInfo): Boolean
 
-  /** Copies the "published" table record to an "unpublished" one.
-    * @note Does not copy the actual tables; this just updates the bookkeeping. */
+  /** Ensures that an "unpublished" table exists, creating it if necessary.
+    * @note Does not copy the actual tables; this just updates the bookkeeping.
+    * @return A [[com.socrata.datacoordinator.truth.metadata.VersionInfo]] for an unpublished version. */
   def ensureUnpublishedCopy(tableInfo: TableInfo): VersionInfo
 
   /** Promotes the current the "published" table record (if it exists) to a "snapshot" one, and promotes the
