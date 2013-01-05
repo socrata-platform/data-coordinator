714583e16be56f9931b92a16754c220d853722dc
Robert J. Macomber <robert.macomber@socrata.com>
2013-01-04 16:44:16 -0800

    Add SLF4j

--- project/Coordinator.scala
+++ project/Coordinator.scala
@@ -9,7 +9,8 @@ object Coordinator {
     libraryDependencies <++= (slf4jVersion) { slf4jVersion =>
       Seq(
         "com.socrata" %% "soql-types" % "0.0.9",
-        "net.sf.opencsv" % "opencsv" % "2.3"
+        "net.sf.opencsv" % "opencsv" % "2.3",
+        "org.slf4j" % "slf4j-simple" % slf4jVersion
       )
     }
   )
