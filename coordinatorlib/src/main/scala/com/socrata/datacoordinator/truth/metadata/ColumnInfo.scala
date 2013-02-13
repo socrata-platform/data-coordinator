8a194a5bd8f3d7c5c243eb3106eaf2a732c7aca6
Robert J. Macomber <robert.macomber@socrata.com>
2013-02-13 13:53:35 -0800

    Add "unanchored" copy and column info, which do not reference their parents

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/metadata/ColumnInfo.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/metadata/ColumnInfo.scala
@@ -9,8 +9,7 @@ import com.rojoma.json.ast.JValue
 
 import com.socrata.datacoordinator.id.ColumnId
 
-trait ColumnInfo extends Product {
-  val copyInfo: CopyInfo
+trait UnanchoredColumnInfo extends Product {
   val systemId: ColumnId
   val logicalName: String
   val typeName: String
@@ -18,22 +17,73 @@ trait ColumnInfo extends Product {
   val isSystemPrimaryKey: Boolean
   val isUserPrimaryKey: Boolean
 
+  protected def canonicalType: Class[_ <: UnanchoredColumnInfo]
+
   lazy val physicalColumnBase = physicalColumnBaseBase + "_" + systemId.underlying
 
   override final def hashCode = ScalaRunTime._hashCode(this)
   override final def productPrefix = "ColumnInfo"
   override final def toString = ScalaRunTime._toString(this)
   override final def equals(o: Any) = o match {
-    case that: ColumnInfo =>
+    case that if canonicalType.isInstance(that) =>
       ScalaRunTime._equals(this, that)
     case _ =>
       false
   }
+}
 
+case class SimpleUnanchoredColumnInfo(systemId: ColumnId, logicalName: String, typeName: String, physicalColumnBaseBase: String, isSystemPrimaryKey: Boolean, isUserPrimaryKey: Boolean) extends UnanchoredColumnInfo {
+  final def canonicalType = classOf[UnanchoredColumnInfo]
+}
+
+object UnanchoredColumnInfo {
+  implicit object jCodec extends JsonCodec[UnanchoredColumnInfo] {
+    val systemIdVar = Variable[ColumnId]
+    val logicalNameVar = Variable[String]
+    val typeNameVar = Variable[String]
+    val physicalColumnBaseBaseVar = Variable[String]
+    val isSystemPrimaryKeyVar = Variable[Boolean]
+    val isUserPrimaryKeyVar = Variable[Boolean]
+
+    val PColumnInfo = PObject(
+      "sid" -> systemIdVar,
+      "name" -> logicalNameVar,
+      "type" -> typeNameVar,
+      "base" -> physicalColumnBaseBaseVar,
+      "spk" -> isSystemPrimaryKeyVar,
+      "upk" -> isUserPrimaryKeyVar
+    )
+
+    def encode(x: UnanchoredColumnInfo) = PColumnInfo.generate(
+      systemIdVar := x.systemId,
+      logicalNameVar := x.logicalName,
+      typeNameVar := x.typeName,
+      physicalColumnBaseBaseVar := x.physicalColumnBaseBase,
+      isSystemPrimaryKeyVar := x.isSystemPrimaryKey,
+      isUserPrimaryKeyVar := x.isUserPrimaryKey
+    )
+
+    def decode(x: JValue) = PColumnInfo.matches(x) map { results =>
+      SimpleUnanchoredColumnInfo(
+        systemId = systemIdVar(results),
+        logicalName = logicalNameVar(results),
+        typeName = typeNameVar(results),
+        physicalColumnBaseBase = physicalColumnBaseBaseVar(results),
+        isSystemPrimaryKey = isSystemPrimaryKeyVar(results),
+        isUserPrimaryKey = isUserPrimaryKeyVar(results)
+      )
+    }
+  }
+}
+
+trait ColumnInfo extends UnanchoredColumnInfo {
+  val copyInfo: CopyInfo
   def withCopyInfo(ci: CopyInfo) = SimpleColumnInfo(ci, systemId, logicalName, typeName, physicalColumnBaseBase, isSystemPrimaryKey, isUserPrimaryKey)
 }
 
-case class SimpleColumnInfo(copyInfo: CopyInfo, systemId: ColumnId, logicalName: String, typeName: String, physicalColumnBaseBase: String, isSystemPrimaryKey: Boolean, isUserPrimaryKey: Boolean) extends ColumnInfo
+case class SimpleColumnInfo(copyInfo: CopyInfo, systemId: ColumnId, logicalName: String, typeName: String, physicalColumnBaseBase: String, isSystemPrimaryKey: Boolean, isUserPrimaryKey: Boolean) extends ColumnInfo {
+  final def canonicalType = classOf[ColumnInfo]
+}
 
 object ColumnInfo {
   implicit object jCodec extends JsonCodec[ColumnInfo] {
--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/metadata/CopyInfo.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/metadata/CopyInfo.scala
@@ -9,30 +9,75 @@ import com.rojoma.json.ast.JValue
 
 import com.socrata.datacoordinator.id.CopyId
 
-trait CopyInfo extends Product {
-  val datasetInfo: DatasetInfo
+trait UnanchoredCopyInfo extends Product {
   val systemId: CopyId
   val copyNumber: Long
   val lifecycleStage: LifecycleStage
   val dataVersion: Long
 
-  lazy val dataTableName = datasetInfo.tableBase + "_" + copyNumber
+  protected def canonicalType: Class[_ <: UnanchoredCopyInfo]
 
   override final def hashCode = ScalaRunTime._hashCode(this)
   override final def productPrefix = "CopyInfo"
   override final def toString = ScalaRunTime._toString(this)
   override final def equals(o: Any) = o match {
-    case that: CopyInfo =>
+    case that if canonicalType.isInstance(that) =>
       ScalaRunTime._equals(this, that)
     case _ =>
       false
   }
+}
+
+case class SimpleUnanchoredCopyInfo(systemId: CopyId, copyNumber: Long, lifecycleStage: LifecycleStage, dataVersion: Long) extends UnanchoredCopyInfo {
+  final def canonicalType = classOf[UnanchoredCopyInfo]
+}
+
+object UnanchoredCopyInfo {
+  implicit object jCodec extends JsonCodec[UnanchoredCopyInfo] {
+    private implicit def lifecycleCodec = LifecycleStage.jCodec
+
+    val systemIdVar = Variable[CopyId]
+    val copyNumberVar = Variable[Long]
+    val lifecycleStageVar = Variable[LifecycleStage]
+    val dataVersionVar = Variable[Long]
+
+    val PCopyInfo = PObject(
+      "sid" -> systemIdVar,
+      "num" -> copyNumberVar,
+      "stage" -> lifecycleStageVar,
+      "ver" -> dataVersionVar
+    )
+
+    def encode(x: UnanchoredCopyInfo) = PCopyInfo.generate(
+      systemIdVar := x.systemId,
+      copyNumberVar := x.copyNumber,
+      lifecycleStageVar := x.lifecycleStage,
+      dataVersionVar := x.dataVersion
+    )
+
+    def decode(x: JValue) = PCopyInfo.matches(x) map { results =>
+      SimpleUnanchoredCopyInfo(
+        systemId = systemIdVar(results),
+        copyNumber = copyNumberVar(results),
+        lifecycleStage = lifecycleStageVar(results),
+        dataVersion = dataVersionVar(results)
+      )
+    }
+  }
+}
+
+trait CopyInfo extends UnanchoredCopyInfo {
+  val datasetInfo: DatasetInfo
+
+  lazy val dataTableName = datasetInfo.tableBase + "_" + copyNumber
 
   def withDatasetInfo(di: DatasetInfo) =
     SimpleCopyInfo(di, systemId, copyNumber, lifecycleStage, dataVersion)
 }
 
-case class SimpleCopyInfo(datasetInfo: DatasetInfo, systemId: CopyId, copyNumber: Long, lifecycleStage: LifecycleStage, dataVersion: Long) extends CopyInfo
+case class SimpleCopyInfo(datasetInfo: DatasetInfo, systemId: CopyId, copyNumber: Long, lifecycleStage: LifecycleStage, dataVersion: Long) extends CopyInfo {
+  final def canonicalType = classOf[CopyInfo]
+}
 
 object CopyInfo {
   implicit object jCodec extends JsonCodec[CopyInfo] {
@@ -45,7 +90,7 @@ object CopyInfo {
     val dataVersionVar = Variable[Long]
 
     val PCopyInfo = PObject(
-      "di" -> datasetInfoVar,
+      "id" -> datasetInfoVar,
       "sid" -> systemIdVar,
       "num" -> copyNumberVar,
       "stage" -> lifecycleStageVar,
