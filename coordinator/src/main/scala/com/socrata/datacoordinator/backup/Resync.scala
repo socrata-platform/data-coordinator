7e95742f6aa931f57f0cdf8d1bd68054fd50d6ec
Robert J. Macomber <robert.macomber@socrata.com>
2013-02-14 15:47:31 -0800

    Add a real exception for Resync
    Backup still catches everything because unchecked exceptions :(
    
    But this shall improve.

--- coordinator/src/main/scala/com/socrata/datacoordinator/backup/Resync.scala
+++ coordinator/src/main/scala/com/socrata/datacoordinator/backup/Resync.scala
@@ -1,8 +1,10 @@
 package com.socrata.datacoordinator.backup
 
+class Resync(msg: String) extends RuntimeException(msg)
+
 object Resync {
   def apply[T](dataset: T, explanation: String)(implicit ev: DatasetIdable[T]): Nothing =
-    throw new Exception("Dataset " + ev.datasetId(dataset).underlying + ": " + explanation)
+    throw new Resync("Dataset " + ev.datasetId(dataset).underlying + ": " + explanation)
 
   @inline final def unless[T: DatasetIdable](dataset: T, condition: Boolean, explanation: => String) {
     if(!condition) apply(dataset, explanation)
