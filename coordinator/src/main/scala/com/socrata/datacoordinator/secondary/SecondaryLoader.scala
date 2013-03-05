373c92117984f140f7e9f9fe2f71932827c1fdae
Robert J. Macomber <robert.macomber@socrata.com>
2013-03-05 10:22:20 -0800

    Make SecondaryLoader nicer to read

--- coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryLoader.scala
+++ coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryLoader.scala
@@ -7,6 +7,7 @@ import java.net.URLClassLoader
 import com.rojoma.json.util.{JsonUtil, AutomaticJsonCodecBuilder, JsonKey}
 import com.rojoma.json.io.JsonReaderException
 import scala.collection.immutable.VectorBuilder
+import scala.util.control.ControlThrowable
 
 case class SecondaryDescription(@JsonKey("class") className: String)
 object SecondaryDescription {
@@ -22,41 +23,42 @@ class SecondaryLoader(parentClassLoader: ClassLoader) {
     })).getOrElse(Array.empty).toSeq
     val result = new VectorBuilder[Secondary[_]]
     for(jar <- jars) {
-      val cl = new URLClassLoader(Array(jar.toURI.toURL), parentClassLoader)
-      val stream = cl.getResourceAsStream("secondary-manifest.json")
+      log.info("Loading secondary from " + jar.getAbsolutePath)
       try {
-        if(stream != null) {
-            JsonUtil.readJson[SecondaryDescription](new InputStreamReader(stream, "UTF-8")) match {
-              case Some(desc) =>
-                val instance = try {
-                  val cls = cl.loadClass(desc.className)
-                  if(!classOf[Secondary[_]].isAssignableFrom(cls)) {
-                    log.warn(cls + " does not name a subclass of Secondary")
-                    None
-                  } else {
-                    Some(cls.newInstance().asInstanceOf[Secondary[_]])
-                  }
-                } catch {
-                  case e: Exception =>
-                    log.warn("Unable to create a new instance of " + desc.className, e)
-                    None
-                }
-                instance.foreach(result += _)
-              case None =>
-                log.warn("Unable to parse a SecondaryDescription from " + jar.getAbsolutePath)
+        val cl = new URLClassLoader(Array(jar.toURI.toURL), parentClassLoader)
+        val stream = cl.getResourceAsStream("secondary-manifest.json")
+        if(stream == null) throw Nope("No secondary-manifest.json in " + jar.getAbsolutePath)
+        try {
+          val desc = try {
+            JsonUtil.readJson[SecondaryDescription](new InputStreamReader(stream, "UTF-8")).getOrElse {
+              throw Nope("Unable to parse a SecondaryDescription from " + jar.getAbsolutePath)
             }
-        } else {
-            log.warn("No secondary-manifest.json in " + jar.getAbsolutePath)
+          } catch {
+              case e: JsonReaderException =>
+                throw Nope("Unable to parse " + jar.getAbsolutePath + " as JSON", e)
+          }
+          val cls =
+            try { cl.loadClass(desc.className) }
+            catch { case e: Exception => throw Nope("Unable to load class " + desc.className + " from " + jar.getAbsolutePath, e) }
+          val ctor =
+            try { cls.getConstructor() }
+            catch { case e: Exception => throw Nope("Unable to find constructor for " + desc.className + " from " + jar.getAbsolutePath, e) }
+          val instance =
+            try { ctor.newInstance().asInstanceOf[Secondary[_]] }
+            catch { case e: Exception => throw Nope("Unable to create a new instance of " + desc.className, e) }
+          result += instance
+        } finally {
+          stream.close()
         }
       } catch {
-        case e: JsonReaderException =>
-          log.warn("Unable to parse " + jar.getAbsolutePath + " as JSON", e)
-      } finally {
-        if(stream != null) stream.close()
+        case Nope(msg, null) => log.warn(msg)
+        case Nope(msg, ex) => log.warn(msg, ex)
       }
     }
     result.result()
   }
+
+  private case class Nope(message: String, cause: Throwable = null) extends Throwable(message, cause) with ControlThrowable
 }
 
 case class LoadedSecondary(instance: Secondary[_])
