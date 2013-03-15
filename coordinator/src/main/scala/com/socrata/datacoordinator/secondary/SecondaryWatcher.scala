efe9a80de9dddcb17b6caf37ed83007bcaa44eec
Robert J. Macomber <robert.macomber@socrata.com>
2013-03-15 10:18:35 -0700

    Rework the main loop in secondary watcher
    * Hoist the loop itself
    * Hook SIGINT and SIGTERM
    * Make sleep time configurable

--- coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryWatcher.scala
+++ coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryWatcher.scala
@@ -18,55 +18,89 @@ import com.socrata.datacoordinator.common.soql.{SoQLRowLogCodec, SoQLRep}
 import com.socrata.soql.types.SoQLType
 import com.socrata.soql.environment.TypeName
 import org.slf4j.LoggerFactory
+import sun.misc.{Signal, SignalHandler}
+import java.util.concurrent.atomic.AtomicBoolean
 
-class SecondaryWatcher[CT, CV](ds: DataSource, secondaries: Map[String, Secondary[CV]], repFor: ColumnInfo => SqlColumnReadRep[CT, CV], codecFactory: () => RowLogCodec[CV], pause: Duration) {
-  val log = LoggerFactory.getLogger(classOf[SecondaryWatcher[CT,CV]])
+class SecondaryWatcher[CT, CV](ds: DataSource, secondaries: Map[String, Secondary[CV]], repFor: ColumnInfo => SqlColumnReadRep[CT, CV], codecFactory: () => RowLogCodec[CV]) {
+  import SecondaryWatcher.log
+
   def run() {
-    while(true) {
-      log.trace("Tick")
-      using(ds.getConnection()) { conn =>
-        conn.setAutoCommit(false)
-        val globalLog = new PostgresGlobalLogPlayback(conn, forBackup = false)
-        val secondaryManifest = new SqlSecondaryManifest(conn)
-        val pb = new PlaybackToSecondary(conn, secondaryManifest, repFor)
-        val dsmr = new PostgresDatasetMapReader(conn)
-        for(job <- globalLog.pendingJobs()) {
-          dsmr.datasetInfo(job.datasetId) match {
-            case Some(datasetInfo) =>
-              for {
-                store <- secondaryManifest.stores(job.datasetId).keys
-                secondary <- secondaries.get(store)
-              } {
-                log.info("Syncing {} (#{}) into {}", Array[AnyRef](datasetInfo.datasetName, job.datasetId.underlying.asInstanceOf[AnyRef], store))
-                val delogger = new SqlDelogger(conn, datasetInfo.logTableName, codecFactory)
-                pb(job.datasetId, NamedSecondary(store, secondary), dsmr, delogger)
-              }
-            case None =>
-              for {
-                store <- secondaryManifest.stores(job.datasetId).keys
-                secondary <- secondaries.get(store)
-              } {
-                log.info("Dropping dataset #{} from {}", job.datasetId.underlying, store)
-                pb.drop(NamedSecondary(store, secondary), job.datasetId)
-              }
-          }
-          globalLog.finishedJob(job)
-          conn.commit()
+    using(ds.getConnection()) { conn =>
+      conn.setAutoCommit(false)
+      val globalLog = new PostgresGlobalLogPlayback(conn, forBackup = false)
+      val secondaryManifest = new SqlSecondaryManifest(conn)
+      val pb = new PlaybackToSecondary(conn, secondaryManifest, repFor)
+      val dsmr = new PostgresDatasetMapReader(conn)
+      for(job <- globalLog.pendingJobs()) {
+        dsmr.datasetInfo(job.datasetId) match {
+          case Some(datasetInfo) =>
+            for {
+              store <- secondaryManifest.stores(job.datasetId).keys
+              secondary <- secondaries.get(store)
+            } {
+              log.info("Syncing {} (#{}) into {}", Array[AnyRef](datasetInfo.datasetName, job.datasetId.underlying.asInstanceOf[AnyRef], store))
+              val delogger = new SqlDelogger(conn, datasetInfo.logTableName, codecFactory)
+              pb(job.datasetId, NamedSecondary(store, secondary), dsmr, delogger)
+            }
+          case None =>
+            for {
+              store <- secondaryManifest.stores(job.datasetId).keys
+              secondary <- secondaries.get(store)
+            } {
+              log.info("Dropping dataset #{} from {}", job.datasetId.underlying, store)
+              pb.drop(NamedSecondary(store, secondary), job.datasetId)
+            }
         }
+        globalLog.finishedJob(job)
+        conn.commit()
       }
-      Thread.sleep(pause.toMillis)
     }
   }
 }
 
 object SecondaryWatcher extends App {
+  val log = LoggerFactory.getLogger(classOf[SecondaryWatcher[_,_]])
+
   val config = ConfigFactory.load().getConfig("com.socrata.secondary-watcher")
   val (dataSource, _) = DataSourceFromConfig(config)
   val secondaries = SecondaryLoader.load(config.getConfig("secondary.configs"), new File(config.getString("secondary.path"))).asInstanceOf[Map[String, Secondary[Any]]]
   def repFor(ci: ColumnInfo) =
     SoQLRep.sqlRepFactories(SoQLType.typesByName(TypeName(ci.typeName)))(ci.physicalColumnBaseBase)
 
-  val w = new SecondaryWatcher[SoQLType, Any](dataSource, secondaries, repFor, () => SoQLRowLogCodec, 1.second)
+  val pause = config.getMilliseconds("sleep-time").longValue.millis
 
-  w.run()
+  val w = new SecondaryWatcher[SoQLType, Any](dataSource, secondaries, repFor, () => SoQLRowLogCodec)
+
+  val SIGTERM = new Signal("TERM")
+  val SIGINT = new Signal("INT")
+
+  val signalled = new AtomicBoolean(false)
+
+  val signalHandler = new SignalHandler {
+    val firstSignal = new java.util.concurrent.atomic.AtomicBoolean(true)
+    def handle(signal: Signal) {
+      if(signalled.getAndSet(true)) log.info("Shutdown already in progress")
+      else log.info("Signalling main thread to shut down")
+    }
+  }
+
+  var oldSIGTERM: SignalHandler = null
+  var oldSIGINT: SignalHandler = null
+  try {
+    log.info("Hooking SIGTERM and SIGINT")
+    oldSIGTERM = Signal.handle(SIGTERM, signalHandler)
+    oldSIGINT = Signal.handle(SIGINT, signalHandler)
+
+    while(!signalled.get()) {
+      log.trace("Tick")
+      w.run()
+      Thread.sleep(pause.toMillis)
+    }
+  } finally {
+    log.info("Un-hooking SIGTERM and SIGINT")
+    if(oldSIGTERM != null) Signal.handle(SIGTERM, oldSIGTERM)
+    if(oldSIGTERM != null) Signal.handle(SIGINT, oldSIGINT)
+  }
+
+  secondaries.values.foreach(_.shutdown())
 }
