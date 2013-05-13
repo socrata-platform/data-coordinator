3ea696a3d32f605b5b5f711d95366d7ee2653423
Robert J. Macomber <robert.macomber@socrata.com>
2013-05-13 12:51:32 -0700

    Change RowDataProvider to use AtomicLongs instead of synchronized blocks
    One behaviour change: finish() is no longer idempotent.

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/util/RowDataProvider.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/util/RowDataProvider.scala
@@ -1,22 +1,22 @@
 package com.socrata.datacoordinator.util
 
 import com.socrata.datacoordinator.id.{RowVersion, RowId}
+import java.util.concurrent.atomic.AtomicLong
 
 class RowDataProvider(initial: Long) {
-  private var next = initial
-  private var finished = false
+  private val next = new AtomicLong(initial)
 
-  def allocate() = synchronized {
-    if(finished) throw new IllegalStateException("Already finished")
-    val result = next
-    next += 1
-    result
+  def allocate(): Long = {
+    while(true) {
+      val result = next.get
+      if(result == -1) throw new IllegalStateException("Already finished")
+      if(next.compareAndSet(result, result + 1)) return result
+    }
+    sys.error("Can't get here")
   }
 
-  def finish(): Long = synchronized {
-    finished = true
-    next
-  }
+  def finish(): Long =
+    next.getAndSet(-1)
 
   def release() {}
 }
