d059d6e97e63dd803c1ecc7b648b010dc2026c6c
Robert J. Macomber <robert.macomber@socrata.com>
2013-05-17 06:26:50 -0700

    Check for RowDataProvider#finish being called more than once

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/util/RowDataProvider.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/util/RowDataProvider.scala
@@ -6,17 +6,23 @@ import java.util.concurrent.atomic.AtomicLong
 class RowDataProvider(initial: Long) {
   private val next = new AtomicLong(initial)
 
+  private def alreadyFinished() =
+    throw new IllegalStateException("Already finished")
+
   def allocate(): Long = {
     while(true) {
       val result = next.get
-      if(result == -1) throw new IllegalStateException("Already finished")
+      if(result == -1) alreadyFinished()
       if(next.compareAndSet(result, result + 1)) return result
     }
     sys.error("Can't get here")
   }
 
-  def finish(): Long =
-    next.getAndSet(-1)
+  def finish(): Long = {
+    val result = next.getAndSet(-1)
+    if(result == -1) alreadyFinished()
+    result
+  }
 
   def release() {}
 }
