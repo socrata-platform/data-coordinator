a2024e062b1dce1630a778bcdeea486dd989827a
Andrew Burnell <andrew.burnell@socrata.com>
2014-02-07 23:56:39 +0000

    Deleted CreateDatabase object for the truth store; MigrateSchema should be used instead

--- coordinator/src/main/scala/com/socrata/datacoordinator/primary/CreateDatabase.scala
+++
@@ -1,7 +0,0 @@
-package com.socrata.datacoordinator.primary
-
-import com.socrata.datacoordinator.common.DatabaseCreator
-
-object CreateDatabase extends App {
-  DatabaseCreator("com.socrata.coordinator.common.database")
-}
