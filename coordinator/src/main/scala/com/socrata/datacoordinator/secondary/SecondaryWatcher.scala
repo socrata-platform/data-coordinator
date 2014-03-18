socrata-platform/data-coordinator

commit a6f9e35fcd344d02cfce68c07ef404c5ae6a2c08

Enabling the use of multiple concurrent secondary watcher processes.

coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryWatcher.scala
@@ -1,7 +1,7 @@
 package com.socrata.datacoordinator.secondary
 
 import com.rojoma.simplearm.Managed
-
+import scala.concurrent.duration._
 import com.rojoma.simplearm.util._
 import com.socrata.datacoordinator.secondary.sql.SqlSecondaryConfig
 import com.typesafe.config.{Config, ConfigFactory}
@@ -18,15 +18,19 @@ import org.apache.log4j.PropertyConfigurator
 import com.socrata.thirdparty.typesafeconfig.Propertizer
 import scala.concurrent.duration.Duration
 import com.socrata.datacoordinator.service.{SecondaryConfig => ServiceSecondaryConfig}
+import scala.util.Random
+import java.util.UUID
 
-class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseType[CT, CV]]) {
+class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseType[CT, CV]], claimantId: UUID, claimTimeout: FiniteDuration) {
   import SecondaryWatcher.log
+  private val rand = new Random()
+  // splay the sleep time +/- 5s to prevent watchers from getting in lock step
+  private val nextRuntimeSplay = (rand.nextInt(10000) - 5000).toLong
 
   def run(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider, secondary: NamedSecondary[CT, CV]) {
     import u._
 
-    val jobs = secondaryManifest.findDatasetsNeedingReplication(secondary.storeId)
-    for(job <- jobs) {
+    for(job <- secondaryManifest.claimDatasetNeedingReplication(secondary.storeId, claimantId, claimTimeout)) {
       log.info("Syncing {} into {}", job.datasetId, secondary.storeId)
       try {
         playbackToSecondary(secondary, job)
@@ -34,12 +38,25 @@ class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseTyp
         case e: Exception =>
           log.error("Unexpected exception while updating dataset {} in secondary {}; marking it as broken", job.datasetId.asInstanceOf[AnyRef], secondary.storeId, e)
           secondaryManifest.markSecondaryDatasetBroken(job)
+      } finally {
+        try {
+          secondaryManifest.releaseClaimedDataset(job)
+        } catch {
+          case e: Exception =>
+            log.error("Unexpected exception while releasing claim on dataset {} in secondary {}", job.datasetId.asInstanceOf[AnyRef], secondary.storeId, e)
+        }
       }
     }
   }
 
+  def cleanOphanedJobs(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider, secondary: NamedSecondary[CT, CV]) {
+    import u._
+    secondaryManifest.cleanOrphanedClaimedDatasets(secondary.storeId, claimantId)
+  }
+
+
   private def maybeSleep(storeId: String, nextRunTime: DateTime, finished: CountDownLatch): Boolean = {
-    val remainingTime = nextRunTime.getMillis - System.currentTimeMillis()
+    val remainingTime = nextRunTime.getMillis - System.currentTimeMillis() - nextRuntimeSplay
     finished.await(remainingTime, TimeUnit.MILLISECONDS)
   }
 
@@ -58,6 +75,13 @@ class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseTyp
   }
 
   def mainloop(secondaryConfigInfo: SecondaryConfigInfo, secondary: Secondary[CT, CV], finished: CountDownLatch) {
+    // Clean up any orphaned jobs which may have been created by this watcher
+    // This needs to be done outside the loop such that it's only executed once.  It also requires access to u.secondaryManifest
+    for { u <- universe } yield {
+      import u.secondaryManifest
+      secondaryManifest.cleanOrphanedClaimedDatasets(secondaryConfigInfo.storeId, claimantId)
+    }
+
     var lastWrote = new DateTime(0L)
     var nextRunTime = bestNextRunTime(secondaryConfigInfo.storeId,
       secondaryConfigInfo.nextRunTime,
@@ -95,7 +119,10 @@ class SecondaryWatcherConfig(config: Config, root: String) {
   val database = new DataSourceConfig(config, k("database"))
   val secondaryConfig = new ServiceSecondaryConfig(config.getConfig(k("secondary")))
   val instance = config.getString(k("instance"))
+  val watcherId = UUID.fromString(config.getString(k("watcher-id")))
+  val claimTimeout = config.getMilliseconds(k("claim-timeout")).longValue.millis
   val tmpdir = new File(config.getString(k("tmpdir"))).getAbsoluteFile
+
 }
 
 object SecondaryWatcher extends App { self =>
@@ -126,7 +153,7 @@ object SecondaryWatcher extends App { self =>
       NullCache
     )
 
-    val w = new SecondaryWatcher(common.universe)
+    val w = new SecondaryWatcher(common.universe, config.watcherId, config.claimTimeout)
 
     val SIGTERM = new Signal("TERM")
     val SIGINT = new Signal("INT")

coordinatorlib/src/main/resources/com.socrata.datacoordinator.truth.schema/20140310-enable-multiple-watcher-processes.xml
@@ -1,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<databaseChangeLog xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                   xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog
+    http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd">
+
+    <changeSet author="Brian Oldfield" id="20140206-enable-multiple-watcher-processes">
+        <preConditions onFail="MARK_RAN">
+            <not><columnExists columnName="claimant_id" tableName="secondary_manifest" /></not>
+            <not><columnExists columnName="claimed_at" tableName="secondary_manifest" /></not>
+        </preConditions>
+
+        <addColumn tableName="secondary_manifest">
+            <column name="claimant_id" type="UUID" />
+        </addColumn>
+        <!-- Create partial index covering (claimant_id, store_id, dataset_system_id) -->
+        <sql>
+            CREATE INDEX secondary_manifest_claimed_job
+            ON secondary_manifest(claimant_id, store_id, dataset_system_id)
+            WHERE claimant_id IS NOT NULL;
+        </sql>
+
+        <addColumn tableName="secondary_manifest">
+            <column name="claimed_at" type="TIMESTAMP WITH TIME ZONE" />
+        </addColumn>
+
+        <rollback>
+            <dropIndex indexName="secondary_manifest_claimed_job" tableName="secondary_manifest" />
+            <dropColumn tableName="secondary_manifest" columnName="claimant_id" />
+            <dropColumn tableName="secondary_manifest" columnName="claimed_at" />
+        </rollback>
+    </changeSet>
+
+</databaseChangeLog>

coordinatorlib/src/main/resources/com.socrata.datacoordinator.truth.schema/migrate.xml
@@ -5,5 +5,6 @@
         xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd">
 
     <include file="com.socrata.datacoordinator.truth.schema/20140206-metadata-bootstrap.xml"/>
+    <include file="com.socrata.datacoordinator.truth.schema/20140310-enable-multiple-watcher-processes.xml"/>
 
 </databaseChangeLog>

coordinatorlib/src/main/scala/com/socrata/datacoordinator/secondary/PlaybackToSecondary.scala
@@ -108,6 +108,7 @@ class PlaybackToSecondary[CT, CV](u: Universe[CT, CV] with Commitable with Secon
                          job: SecondaryRecord)
   {
     private val datasetId = job.datasetId
+    private val claimantId = job.claimantId
     private var currentCookie = job.initialCookie
     private var currentLifecycleStage = job.startingLifecycleStage
     private val datasetMapReader = u.datasetMapReader
@@ -276,7 +277,7 @@ class PlaybackToSecondary[CT, CV](u: Universe[CT, CV] with Commitable with Secon
     }
 
     def updateSecondaryMap(newLastDataVersion: Long, newLifecycleStage: metadata.LifecycleStage) {
-      u.secondaryManifest.completedReplicationTo(secondary.storeId, datasetId, newLastDataVersion, newLifecycleStage, currentCookie)
+      u.secondaryManifest.completedReplicationTo(secondary.storeId, claimantId, datasetId, newLastDataVersion, newLifecycleStage, currentCookie)
       u.commit()
     }
 

coordinatorlib/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryManifest.scala
@@ -3,8 +3,10 @@ package secondary
 
 import com.socrata.datacoordinator.id.DatasetId
 import com.socrata.datacoordinator.truth.metadata
+import scala.concurrent.duration.FiniteDuration
+import java.util.UUID
 
-case class SecondaryRecord(storeId: String, datasetId: DatasetId, startingDataVersion: Long, startingLifecycleStage: metadata.LifecycleStage, endingDataVersion: Long, initialCookie: Option[String])
+case class SecondaryRecord(storeId: String, claimantId: UUID, datasetId: DatasetId, startingDataVersion: Long, startingLifecycleStage: metadata.LifecycleStage, endingDataVersion: Long, initialCookie: Option[String])
 class DatasetAlreadyInSecondary(val storeId: String, val DatasetId: DatasetId) extends Exception
 
 trait SecondaryManifest {
@@ -16,9 +18,11 @@ trait SecondaryManifest {
   def datasets(storeId: String): Map[DatasetId, Long]
   def stores(datasetId: DatasetId): Map[String, Long]
 
-  def findDatasetsNeedingReplication(storeId: String, limit: Int = 1000): Seq[SecondaryRecord]
+  def cleanOrphanedClaimedDatasets(storeId: String, claimantId: UUID)
+  def claimDatasetNeedingReplication(storeId: String, claimantId: UUID, claimTimeout: FiniteDuration): Option[SecondaryRecord]
+  def releaseClaimedDataset(job: SecondaryRecord)
   def markSecondaryDatasetBroken(job: SecondaryRecord)
-  def completedReplicationTo(storeId: String, datasetId: DatasetId, dataVersion: Long, lifecycleStage: metadata.LifecycleStage, newCookie: Option[String])
+  def completedReplicationTo(storeId: String, claimantId: UUID, datasetId: DatasetId, dataVersion: Long, lifecycleStage: metadata.LifecycleStage, newCookie: Option[String])
 }
 
 case class NamedSecondary[CT, CV](storeId: String, store: Secondary[CT, CV])

coordinatorlib/src/main/scala/com/socrata/datacoordinator/secondary/sql/SqlSecondaryConfig.scala
@@ -11,7 +11,12 @@ class SqlSecondaryConfig(conn: Connection, timingReport: TimingReport) extends S
   private def t = timingReport
 
   def lookup(storeId: String): Option[SecondaryConfigInfo] =
-    using(conn.prepareStatement("SELECT store_id, next_run_time, interval_in_seconds FROM secondary_stores_config WHERE store_id = ?")) { stmt =>
+    using(conn.prepareStatement(
+        """SELECT store_id
+          |  ,next_run_time
+          |  ,interval_in_seconds
+          |FROM secondary_stores_config
+          |WHERE store_id = ?""".stripMargin)) { stmt =>
       stmt.setString(1, storeId)
       using(t("lookup-store-config", "store_id" -> storeId)(stmt.executeQuery())) { rs =>
         if(rs.next()) {
@@ -23,7 +28,11 @@ class SqlSecondaryConfig(conn: Connection, timingReport: TimingReport) extends S
     }
 
   def create(secondaryInfo: SecondaryConfigInfo): SecondaryConfigInfo =
-    using(conn.prepareStatement("INSERT INTO secondary_stores_config (store_id, next_run_time, interval_in_seconds) VALUES (?, ?, ?)")) { stmt =>
+    using(conn.prepareStatement(
+      """INSERT INTO secondary_stores_config (store_id
+        |  ,next_run_time
+        |  ,interval_in_seconds
+        |) VALUES (?, ?, ?)""".stripMargin)) { stmt =>
       stmt.setString(1, secondaryInfo.storeId)
       stmt.setTimestamp(2, new Timestamp(secondaryInfo.nextRunTime.getMillis))
       stmt.setInt(3, secondaryInfo.runIntervalSeconds)
@@ -32,7 +41,10 @@ class SqlSecondaryConfig(conn: Connection, timingReport: TimingReport) extends S
     }
 
   def updateNextRunTime(storeId: String, newNextRunTime: DateTime) {
-    using(conn.prepareStatement("UPDATE secondary_stores_config SET next_run_time = ? WHERE store_id = ?")) { stmt =>
+    using(conn.prepareStatement(
+      """UPDATE secondary_stores_config
+        |SET next_run_time = ?
+        |WHERE store_id = ?""".stripMargin)) { stmt =>
       stmt.setTimestamp(1, new Timestamp(newNextRunTime.getMillis))
       stmt.setString(2, storeId)
       t("update-next-runtime", "store-id" -> storeId)(stmt.execute())

coordinatorlib/src/main/scala/com/socrata/datacoordinator/secondary/sql/SqlSecondaryManifest.scala
@@ -2,6 +2,7 @@ package com.socrata.datacoordinator.secondary
 package sql
 
 import java.sql.{Types, Connection}
+import java.util.UUID
 
 import com.rojoma.simplearm.util._
 
@@ -10,6 +11,7 @@ import com.socrata.datacoordinator.id.sql._
 import scala.collection.immutable.VectorBuilder
 import com.socrata.datacoordinator.truth.metadata
 import com.socrata.datacoordinator.util.PostgresUniqueViolation
+import scala.concurrent.duration.FiniteDuration
 
 class SqlSecondaryManifest(conn: Connection) extends SecondaryManifest {
   def readLastDatasetInfo(storeId: String, datasetId: DatasetId): Option[(Long, Option[String])] =
@@ -93,50 +95,133 @@ class SqlSecondaryManifest(conn: Connection) extends SecondaryManifest {
     }
   }
 
-  def findDatasetsNeedingReplication(storeId: String, limit: Int): Seq[SecondaryRecord] =
-    using(conn.prepareStatement("SELECT dataset_system_id, latest_secondary_data_version, latest_secondary_lifecycle_stage, latest_data_version, cookie FROM secondary_manifest WHERE store_id = ? AND broken_at IS NULL AND latest_data_version > latest_secondary_data_version ORDER BY went_out_of_sync_at LIMIT ?")) { stmt =>
+  def claimDatasetNeedingReplication(storeId: String, claimantId: UUID, claimTimeout: FiniteDuration):Option[SecondaryRecord] = {
+    val job = using(conn.prepareStatement(
+      """SELECT dataset_system_id
+        |  ,latest_secondary_data_version
+        |  ,latest_secondary_lifecycle_stage
+        |  ,latest_data_version
+        |  ,cookie
+        |FROM secondary_manifest
+        |WHERE store_id = ?
+        |  AND broken_at IS NULL
+        |  AND latest_data_version > latest_secondary_data_version
+        |  AND (claimant_id is NULL
+        |    OR claimed_at < (CURRENT_TIMESTAMP - CAST (? AS INTERVAL)))
+        |ORDER BY went_out_of_sync_at
+        |LIMIT 1
+        |FOR UPDATE""".stripMargin)) { stmt =>
       stmt.setString(1, storeId)
-      stmt.setInt(2, limit)
+      stmt.setString(2, claimTimeout.toMillis + " milliseconds")
       using(stmt.executeQuery()) { rs =>
-        val results = new VectorBuilder[SecondaryRecord]
-        while(rs.next()) {
-          results += SecondaryRecord(
+        if(rs.next()) {
+          val j = SecondaryRecord(
             storeId,
+            claimantId,
             rs.getDatasetId("dataset_system_id"),
             startingDataVersion = rs.getLong("latest_secondary_data_version") + 1,
             startingLifecycleStage = rs.getLifecycleStage("latest_secondary_lifecycle_stage"),
             endingDataVersion = rs.getLong("latest_data_version"),
-            initialCookie = Option(rs.getString("cookie"))
-          )
+            initialCookie = Option(rs.getString("cookie")))
+          markDatasetClaimedForReplication(j)
+          Some(j)
         }
-        results.result()
+        else None
       }
     }
+    conn.commit()
+    job
+  }
+
+  def cleanOrphanedClaimedDatasets(storeId: String, claimantId: UUID) {
+    using(conn.prepareStatement(
+      """SELECT  dataset_system_id
+        |  ,latest_secondary_data_version
+        |  ,latest_secondary_lifecycle_stage
+        |  ,latest_data_version
+        |  ,cookie
+        |FROM secondary_manifest
+        |WHERE claimant_id = ?
+        |  AND store_id = ?""".stripMargin)) {stmt =>
+      stmt.setObject(1, claimantId)
+      stmt.setString(2, storeId)
+      using(stmt.executeQuery()) { rs =>
+        while(rs.next()){
+          val j = SecondaryRecord(
+            storeId,
+            claimantId,
+            rs.getDatasetId("dataset_system_id"),
+            startingDataVersion = rs.getLong("latest_secondary_data_version") + 1,
+            startingLifecycleStage = rs.getLifecycleStage("latest_secondary_lifecycle_stage"),
+            endingDataVersion = rs.getLong("latest_data_version"),
+            initialCookie = Option(rs.getString("cookie")))
+          releaseClaimedDataset(j)
+        }
+      }
+    }
+  }
+
+  def markDatasetClaimedForReplication(job: SecondaryRecord) {
+    using(conn.prepareStatement(
+      """UPDATE secondary_manifest
+        |SET claimed_at = CURRENT_TIMESTAMP
+        |  ,claimant_id = ?
+        |WHERE store_id = ?
+        |  AND dataset_system_id = ?""".stripMargin)) { stmt =>
+      stmt.setObject(1, job.claimantId)
+      stmt.setString(2, job.storeId)
+      stmt.setLong(3, job.datasetId.underlying)
+      stmt.executeUpdate()
+    }
+  }
+
+  def releaseClaimedDataset(job: SecondaryRecord) {
+    using(conn.prepareStatement(
+      """UPDATE secondary_manifest
+        |SET claimed_at = NULL
+        |  ,claimant_id = NULL
+        |WHERE claimant_id = ?
+        |  AND store_id = ?
+        |  AND dataset_system_id = ?""".stripMargin)) { stmt =>
+      stmt.setObject(1, job.claimantId)
+      stmt.setString(2, job.storeId)
+      stmt.setLong(3, job.datasetId.underlying)
+      stmt.executeUpdate()
+    }
+  }
+
 
   def markSecondaryDatasetBroken(job: SecondaryRecord) {
-    using(conn.prepareStatement("UPDATE secondary_manifest SET broken_at = CURRENT_TIMESTAMP WHERE store_id = ? AND dataset_system_id = ?")) { stmt =>
+    using(conn.prepareStatement(
+      """UPDATE secondary_manifest
+        |SET broken_at = CURRENT_TIMESTAMP
+        |WHERE store_id = ?
+        |  AND dataset_system_id = ?""".stripMargin)) { stmt =>
       stmt.setString(1, job.storeId)
       stmt.setLong(2, job.datasetId.underlying)
       stmt.executeUpdate()
     }
   }
 
-  def completedReplicationTo(storeId: String, datasetId: DatasetId, dataVersion: Long, lifecycleStage: metadata.LifecycleStage, cookie: Option[String]) {
+  def completedReplicationTo(storeId: String, claimantId: UUID, datasetId: DatasetId, dataVersion: Long, lifecycleStage: metadata.LifecycleStage, cookie: Option[String]) {
     using(conn.prepareStatement(
       """UPDATE secondary_manifest
-        | SET latest_secondary_data_version = ?,
-        |     latest_secondary_lifecycle_stage = CAST(? AS dataset_lifecycle_stage),
-        |     cookie = ?,
-        |     went_out_of_sync_at = CURRENT_TIMESTAMP
-        | WHERE store_id = ? AND dataset_system_id = ?""".stripMargin)) { stmt =>
+        |SET latest_secondary_data_version = ?
+        |  ,latest_secondary_lifecycle_stage = CAST(? AS dataset_lifecycle_stage)
+        |  ,cookie = ?
+        |  ,went_out_of_sync_at = CURRENT_TIMESTAMP
+        |WHERE claimant_id = ?
+        |  AND store_id = ?
+        |  AND dataset_system_id = ?""".stripMargin)) { stmt =>
       stmt.setLong(1, dataVersion)
       stmt.setLifecycleStage(2, lifecycleStage)
       cookie match {
         case Some(c) => stmt.setString(3, c)
         case None => stmt.setNull(3, Types.VARCHAR)
       }
-      stmt.setString(4, storeId)
-      stmt.setDatasetId(5, datasetId)
+      stmt.setObject(4, claimantId)
+      stmt.setString(5, storeId)
+      stmt.setDatasetId(6, datasetId)
       stmt.executeUpdate()
     }
   }

commit ae43802b59fe50700f136e00103025f5ceac8a33

Loop when sending changes to the secondary until there are no more changes to send.
This was broken in a6f9e35fcd344d02cfce68c07ef404c5ae6a2c08, so we were pausing between
each operation which is not ideal.

coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryWatcher.scala
@@ -27,10 +27,10 @@ class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseTyp
   // splay the sleep time +/- 5s to prevent watchers from getting in lock step
   private val nextRuntimeSplay = (rand.nextInt(10000) - 5000).toLong
 
-  def run(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider, secondary: NamedSecondary[CT, CV]) {
+  def run(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider, secondary: NamedSecondary[CT, CV]): Boolean = {
     import u._
 
-    for(job <- secondaryManifest.claimDatasetNeedingReplication(secondary.storeId, claimantId, claimTimeout)) {
+    val foundWorkToDo = for (job <-  secondaryManifest.claimDatasetNeedingReplication(secondary.storeId, claimantId, claimTimeout)) yield {
       log.info("Syncing {} into {}", job.datasetId, secondary.storeId)
       try {
         playbackToSecondary(secondary, job)
@@ -47,6 +47,7 @@ class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseTyp
         }
       }
     }
+    foundWorkToDo.isDefined
   }
 
   def cleanOphanedJobs(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider, secondary: NamedSecondary[CT, CV]) {
@@ -92,8 +93,9 @@ class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseTyp
       for { u <- universe } yield {
         import u._
 
-        run(u, new NamedSecondary(secondaryConfigInfo.storeId, secondary))
-
+        while(run(u, new NamedSecondary(secondaryConfigInfo.storeId, secondary)) && finished.getCount != 0) {
+          // loop until we either have no more work or we are told to exit
+        }
 
         nextRunTime = bestNextRunTime(secondaryConfigInfo.storeId,
           nextRunTime.plus(Seconds.seconds(secondaryConfigInfo.runIntervalSeconds)),