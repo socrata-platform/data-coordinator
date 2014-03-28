17606a992e5d071bafbfb9fe6ec411afc800292b
Marc Slemko <marc.slemko@socrata.com>
2014-03-28 13:36:01 -0700

    Order by id when dropping tables so we can get the proper ordering for foreign key dependencies.

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/universe/sql/SqlTableCleanup.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/universe/sql/SqlTableCleanup.scala
@@ -9,7 +9,7 @@ class SqlTableCleanup(conn: Connection, daysDelay: Int = 1) extends TableCleanup
   val log = org.slf4j.LoggerFactory.getLogger(classOf[SqlTableCleanup])
   def cleanupPendingDrops(): Boolean = {
     using(conn.createStatement()) { stmt =>
-      using(stmt.executeQuery(s"SELECT id, table_name FROM pending_table_drops WHERE queued_at < now() - ('$daysDelay day' :: INTERVAL) ORDER BY queued_at LIMIT 1 FOR UPDATE")) { rs =>
+      using(stmt.executeQuery(s"SELECT id, table_name FROM pending_table_drops WHERE queued_at < now() - ('$daysDelay day' :: INTERVAL) ORDER BY id LIMIT 1 FOR UPDATE")) { rs =>
         if(rs.next()) {
           val id = rs.getLong("id")
           val tableName = rs.getString("table_name")
