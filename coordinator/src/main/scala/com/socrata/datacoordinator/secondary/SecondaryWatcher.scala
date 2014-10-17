7767d07e40c020f7353a8b7483fba9b2d9fdc309
Evan Chan <evan.chan@socrata.com>
2014-10-17 10:29:49 -0700

    More code cleanup

--- coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryWatcher.scala
+++ coordinator/src/main/scala/com/socrata/datacoordinator/secondary/SecondaryWatcher.scala
@@ -1,37 +1,41 @@
 package com.socrata.datacoordinator.secondary
 
 import com.rojoma.simplearm.Managed
-import scala.concurrent.duration._
 import com.rojoma.simplearm.util._
-import com.socrata.datacoordinator.secondary.sql.SqlSecondaryConfig
-import com.typesafe.config.{Config, ConfigFactory}
-import com.socrata.datacoordinator.common.{DataSourceConfig, SoQLCommon, DataSourceFromConfig}
-import java.io.File
-import com.socrata.soql.types.{SoQLValue, SoQLType}
-import org.slf4j.LoggerFactory
-import sun.misc.{Signal, SignalHandler}
-import java.util.concurrent.{Executors, TimeUnit, CountDownLatch}
-import org.joda.time.{DateTime, Seconds}
-import com.socrata.datacoordinator.util.{TimingReport, NullCache, StackedTimingReport, LoggedTimingReport}
-import com.socrata.datacoordinator.truth.universe._
-import org.apache.log4j.PropertyConfigurator
-import com.socrata.thirdparty.typesafeconfig.Propertizer
-import scala.concurrent.duration.Duration
-import com.socrata.datacoordinator.service.{SecondaryConfig => ServiceSecondaryConfig}
-import scala.util.Random
-import java.util.UUID
 import com.socrata.datacoordinator.common.DataSourceFromConfig.DSInfo
+import com.socrata.datacoordinator.common.{DataSourceConfig, SoQLCommon, DataSourceFromConfig}
+import com.socrata.datacoordinator.secondary.sql.SqlSecondaryConfig
+import com.socrata.datacoordinator.service.{SecondaryConfig => ServiceSecondaryConfig}
+import com.socrata.datacoordinator.truth.universe._
+import com.socrata.datacoordinator.util.{TimingReport, NullCache, StackedTimingReport, LoggedTimingReport}
+import com.socrata.soql.types.{SoQLValue, SoQLType}
+import com.socrata.thirdparty.typesafeconfig.Propertizer
+import com.typesafe.config.{Config, ConfigFactory}
+import java.io.File
+import java.util.concurrent.{Executors, TimeUnit, CountDownLatch}
+import java.util.UUID
+import org.apache.log4j.PropertyConfigurator
+import org.joda.time.{DateTime, Seconds}
+import org.slf4j.LoggerFactory
+import scala.concurrent.duration._
+import scala.util.Random
+import sun.misc.{Signal, SignalHandler}
 
-class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseType[CT, CV]], claimantId: UUID, claimTimeout: FiniteDuration, timingReport: TimingReport) {
+class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseType[CT, CV]],
+                               claimantId: UUID,
+                               claimTimeout: FiniteDuration,
+                               timingReport: TimingReport) {
   import SecondaryWatcher.log
   private val rand = new Random()
   // splay the sleep time +/- 5s to prevent watchers from getting in lock step
   private val nextRuntimeSplay = (rand.nextInt(10000) - 5000).toLong
 
-  def run(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider, secondary: NamedSecondary[CT, CV]): Boolean = {
+  def run(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider,
+          secondary: NamedSecondary[CT, CV]): Boolean = {
     import u._
 
-    val foundWorkToDo = for (job <-  secondaryManifest.claimDatasetNeedingReplication(secondary.storeId, claimantId, claimTimeout)) yield {
+    val foundWorkToDo = for (job <- secondaryManifest.claimDatasetNeedingReplication(
+                                      secondary.storeId, claimantId, claimTimeout)) yield {
       log.info("Syncing {} into {}", job.datasetId, secondary.storeId)
       try {
         timingReport(
@@ -42,21 +46,24 @@ class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseTyp
         )(playbackToSecondary(secondary, job))
       } catch {
         case e: Exception =>
-          log.error("Unexpected exception while updating dataset {} in secondary {}; marking it as broken", job.datasetId.asInstanceOf[AnyRef], secondary.storeId, e)
+          log.error("Unexpected exception while updating dataset {} in secondary {}; marking it as broken",
+                    job.datasetId.asInstanceOf[AnyRef], secondary.storeId, e)
           secondaryManifest.markSecondaryDatasetBroken(job)
       } finally {
         try {
           secondaryManifest.releaseClaimedDataset(job)
         } catch {
           case e: Exception =>
-            log.error("Unexpected exception while releasing claim on dataset {} in secondary {}", job.datasetId.asInstanceOf[AnyRef], secondary.storeId, e)
+            log.error("Unexpected exception while releasing claim on dataset {} in secondary {}",
+                      job.datasetId.asInstanceOf[AnyRef], secondary.storeId, e)
         }
       }
     }
     foundWorkToDo.isDefined
   }
 
-  def cleanOphanedJobs(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider, secondary: NamedSecondary[CT, CV]) {
+  def cleanOphanedJobs(u: Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider,
+                       secondary: NamedSecondary[CT, CV]) {
     import u._
     secondaryManifest.cleanOrphanedClaimedDatasets(secondary.storeId, claimantId)
   }
@@ -71,7 +78,8 @@ class SecondaryWatcher[CT, CV](universe: => Managed[SecondaryWatcher.UniverseTyp
     val remainingTime = (now.getMillis - target.getMillis) / 1000
     if(remainingTime > 0) {
       val diffInIntervals = remainingTime / interval
-      log.warn("{} is behind schedule {}s ({} intervals)", storeId, remainingTime.asInstanceOf[AnyRef], diffInIntervals.asInstanceOf[AnyRef])
+      log.warn("{} is behind schedule {}s ({} intervals)",
+               storeId, remainingTime.asInstanceOf[AnyRef], diffInIntervals.asInstanceOf[AnyRef])
       val newTarget = target.plus(Seconds.seconds(Math.min(diffInIntervals * interval, Int.MaxValue).toInt))
       log.warn("Resetting target time to {}", newTarget)
       newTarget
@@ -151,7 +159,8 @@ class SecondaryWatcherClaimManager(dsInfo: DSInfo, claimantId: UUID, claimTimeou
         lastUpdate = DateTime.now()
       } catch {
         case e: Exception =>
-          log.error("Unexpected exception while updating claimedAt time for secondary sync jobs claimed by watcherId " + claimantId.toString(), e)
+          log.error("Unexpected exception while updating claimedAt time for secondary sync jobs " +
+                    "claimed by watcherId " + claimantId.toString(), e)
       }
       done = awaitEither(finished, updateInterval.toMillis)
     }
@@ -175,7 +184,9 @@ class SecondaryWatcherClaimManager(dsInfo: DSInfo, claimantId: UUID, claimTimeou
 }
 
 object SecondaryWatcher extends App { self =>
-  type UniverseType[CT, CV] = Universe[CT, CV] with SecondaryManifestProvider with PlaybackToSecondaryProvider with SecondaryConfigProvider
+  type UniverseType[CT, CV] = Universe[CT, CV] with SecondaryManifestProvider with
+                                                    PlaybackToSecondaryProvider with
+                                                    SecondaryConfigProvider
 
   val rootConfig = ConfigFactory.load()
   println(rootConfig.root.render())
@@ -192,7 +203,8 @@ object SecondaryWatcher extends App { self =>
   })
 
   for(dsInfo <- DataSourceFromConfig(config.database)) {
-    val secondaries = SecondaryLoader.load(config.secondaryConfig).asInstanceOf[Map[String, Secondary[SoQLType, SoQLValue]]]
+    val secondaries = SecondaryLoader.load(config.secondaryConfig).
+                        asInstanceOf[Map[String, Secondary[SoQLType, SoQLValue]]]
 
     val executor = Executors.newCachedThreadPool()
 
