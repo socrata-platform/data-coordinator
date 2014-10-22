45fbfb557a65ec1d64bfee3fcc468efdee78acb0
Marc Slemko <marc.slemko@socrata.com>
2014-10-22 13:40:22 -0700

    Add support for rewriting date_trunc_xx used in BETWEEN clauses to use rollups.
    This allows rewriting expressions of this particular form to use aggregated date_trunc_xx
    columns in rollup tables:
    
            a BETWEEN date_trunc_xx(b) AND date_trunc_xx(c)
    
    For date_trunc_{y,ym,ymd}.  This is typically used in the WHERE clause.
    
    This also supports rewriting if the rollup has a less granular
    rollup than requested (eg. rollup has ymd and query has ym).

---
+++ src/test/scala/com/socrata/querycoordinator/TestQueryRewriterDateTrunc.scala
@@ -0,0 +1,119 @@
+package com.socrata.querycoordinator
+
+import com.socrata.soql.SoQLAnalyzer
+import com.socrata.soql.environment.{ColumnName, TypeName}
+import com.socrata.soql.functions.{SoQLFunctionInfo, SoQLTypeInfo}
+import com.socrata.soql.types.SoQLType
+
+class TestQueryRewriterDateTrunc extends TestQueryRewriterBase {
+  /** Each rollup here is defined by:
+    * - a name
+    * - a soql statement.  Note this must be the mapped statement, ie. non-system columns prefixed by an _, and backtick escaped
+    * - a Seq of the soql types for each column in the rollup selection
+    */
+  val rollups = Seq(
+    ("r_ymd", "SELECT date_trunc_ymd(`_crim-date`), `:wido-ward`, count(*) GROUP BY date_trunc_ymd(`_crim-date`), `:wido-ward`"),
+    ("r_ym", "SELECT date_trunc_ym(`_crim-date`), `:wido-ward`, count(*) GROUP BY date_trunc_ym(`_crim-date`), `:wido-ward`"),
+    ("r_y", "SELECT date_trunc_y(`_crim-date`), `:wido-ward`, count(*) GROUP BY date_trunc_y(`_crim-date`), `:wido-ward`")
+  )
+
+  val rollupInfos = rollups.map { x => new RollupInfo(x._1, x._2)}
+
+  /** Pull in the rollupAnalysis for easier debugging */
+  val rollupAnalysis = rewriter.analyzeRollups(schema, rollupInfos)
+
+  val rollupRawSchemas = rollupAnalysis.mapValues { case analysis =>
+    analysis.selection.values.toSeq.zipWithIndex.map { case (expr, idx) =>
+      rewriter.rollupColumnId(idx) -> expr.typ.canonical
+    }.toMap
+  }
+
+  val rewrittenQueryRymd = "SELECT c2 as ward, sum(c3) as count WHERE c1 BETWEEN date_trunc_ymd('2011-02-01') AND date_trunc_ymd('2012-05-02') GROUP BY c2"
+  val rewrittenQueryAnalysisRymd = analyzeRewrittenQuery("r_ymd", rewrittenQueryRymd)
+
+  val rewrittenQueryRym = "SELECT c2 as ward, sum(c3) as count WHERE c1 BETWEEN date_trunc_ym('2011-02-01') AND date_trunc_ym('2012-05-02') GROUP BY c2"
+  val rewrittenQueryAnalysisRym = analyzeRewrittenQuery("r_ymd", rewrittenQueryRym)
+
+  val rewrittenQueryRy = "SELECT c2 as ward, sum(c3) as count WHERE c1 BETWEEN date_trunc_y('2011-02-01') AND date_trunc_y('2012-05-02') GROUP BY c2"
+  val rewrittenQueryAnalysisRy = analyzeRewrittenQuery("r_y", rewrittenQueryRy)
+
+  /** Analyze a "fake" query that has the rollup table column names in, so we
+    * can use it to compare  with the rewritten one in assertions.
+    */
+  def analyzeRewrittenQuery(rollupName: String, q: String) = {
+    val rewrittenRawSchema = rollupRawSchemas(rollupName)
+
+    val rollupNoopColumnNameMap = rewrittenRawSchema.map { case (k, v) => ColumnName(k) -> k}
+
+    val rollupDsContext = QueryParser.dsContext(rollupNoopColumnNameMap, rewrittenRawSchema) match {
+      case Right(ctx) => ctx
+      case Left(unknownColumnIds) => throw new RuntimeException("Unknown column ids:" + unknownColumnIds)
+    }
+
+    val rewrittenQueryAnalysis = analyzer.analyzeFullQuery(q)(rollupDsContext).mapColumnIds(rollupNoopColumnNameMap)
+    rewrittenQueryAnalysis
+  }
+
+  test("Shouldn't rewrite date_trunc on literals") {
+    val q = "SELECT ward, count(*) as count WHERE crime_date BETWEEN '2004-01-01' AND '2014-01-01' GROUP BY ward"
+    val queryAnalysis = analyzeQuery(q)
+
+    val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
+    rewrites should have size (0)
+  }
+
+  test("Shouldn't rewrite date_trunc on differing aggregations") {
+    val q = "SELECT ward, count(*) as count WHERE crime_date BETWEEN date_trunc_y('2004-01-01') AND date_trunc_ym('2014-01-01') GROUP BY ward"
+    val queryAnalysis = analyzeQuery(q)
+
+    val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
+    rewrites should have size (0)
+  }
+
+  test("date_trunc_ymd") {
+    val q = "SELECT ward, count(*) AS count WHERE " +
+      "crime_date BETWEEN date_trunc_ymd('2011-02-01') AND date_trunc_ymd('2012-05-02') GROUP BY ward"
+    val queryAnalysis = analyzeQuery(q)
+
+    val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
+
+    rewrites should contain key ("r_ymd")
+    rewrites.get("r_ymd").get should equal(rewrittenQueryAnalysisRymd)
+
+    rewrites should have size (1)
+  }
+
+
+  test("date_trunc_ym") {
+    val q = "SELECT ward, count(*) AS count WHERE " +
+      "crime_date BETWEEN date_trunc_ym('2011-02-01') AND date_trunc_ym('2012-05-02') GROUP BY ward"
+    val queryAnalysis = analyzeQuery(q)
+
+    val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
+
+    rewrites should contain key ("r_ymd")
+    rewrites.get("r_ymd").get should equal(rewrittenQueryAnalysisRym)
+    rewrites should contain key ("r_ym")
+    rewrites.get("r_ym").get should equal(rewrittenQueryAnalysisRym)
+
+    rewrites should have size (2)
+  }
+
+
+  test("date_trunc_y") {
+    val q = "SELECT ward, count(*) AS count WHERE " +
+      "crime_date BETWEEN date_trunc_y('2011-02-01') AND date_trunc_y('2012-05-02') GROUP BY ward"
+    val queryAnalysis = analyzeQuery(q)
+
+    val rewrites = rewriter.possibleRewrites(queryAnalysis, rollupAnalysis)
+
+    rewrites should contain key ("r_ymd")
+    rewrites.get("r_ymd").get should equal(rewrittenQueryAnalysisRy)
+    rewrites should contain key ("r_ym")
+    rewrites.get("r_ym").get should equal(rewrittenQueryAnalysisRy)
+    rewrites should contain key ("r_y")
+    rewrites.get("r_y").get should equal(rewrittenQueryAnalysisRy)
+
+    rewrites should have size (3)
+  }
+}
