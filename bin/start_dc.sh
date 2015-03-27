43bfbad3da185eafd27ec9a22305d2ce4a0416c2
Evan Chan <evan.chan@socrata.com>
2015-03-27 11:15:10 -0700

    Fix start scripts

--- bin/start_dc.sh
+++ bin/start_dc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start data coordinator locally and build it if necessary
 BASEDIR=$(dirname $0)/..
-CONFIG=${SODA_CONFIG:-$BASEDIR/../onramp/services/soda2.conf}
+CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
 JARFILE=$BASEDIR/coordinator/target/scala-2.10/coordinator-assembly-*.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt assembly
