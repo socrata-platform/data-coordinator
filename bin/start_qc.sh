67c4f8c1e6c82b43e2c456bcfe035bf278274f3f
Chris Duranti <chris.duranti@socrata.com>
2015-05-14 10:31:36 -0700

    * rename start_qc.sh command so it finds the right jar * refs #42

--- bin/start_qc.sh
+++ bin/start_qc.sh
@@ -2,7 +2,7 @@
 # Starts the query coordinator.
 BASEDIR=$(dirname $0)/..
 CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
-JARFILE=$BASEDIR/query-coordinator-http/target/scala-2.10/query-coordinator-assembly-*.jar
+JARFILE=$BASEDIR/query-coordinator/target/scala-2.10/query-coordinator-assembly-*.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt assembly
 fi
