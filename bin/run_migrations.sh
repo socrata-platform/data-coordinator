f4e78be5cd3270f70d66aaf44a6d67ef47cc4266
Kaida Masaki <kaida.masaki@socrata.com>
2015-10-07 14:08:32 -0700

    Improve multiple jar handling

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # Run data coordinator migrations
 # run_migrations.sh [migrate/undo/redo] [numchanges]
-BASEDIR=$(dirname $0)/..
+BASEDIR=$(dirname "$0")/..
 CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
-JARFILE=$BASEDIR/coordinator/target/scala-2.10/coordinator-assembly-*.jar
-if [ ! -e $JARFILE ]; then
-  cd $BASEDIR && sbt assembly
+
+JARS=( $BASEDIR/coordinator/target/scala-2.10/coordinator-assembly-*.jar )
+# shellcheck disable=SC2012
+JARFILE=$(ls -t "${JARS[@]}" | head -n 1)
+
+if [ ! -e "$JARFILE" ]; then
+    cd "$BASEDIR" && sbt assembly
+    JARS=( $BASEDIR/coordinator/target/scala-2.10/coordinator-assembly-*.jar )
+    # shellcheck disable=SC2012
+    JARFILE=$(ls -t "${JARS[@]}" | head -n 1)
 fi
+
 COMMAND=${1:-migrate}
-echo Running datacoordinator.primary.MigrateSchema $COMMAND $2...
-java -Dconfig.file=$CONFIG -jar $JARFILE com.socrata.datacoordinator.primary.MigrateSchema $COMMAND $2
+echo Running datacoordinator.primary.MigrateSchema "$COMMAND" "$2"...
+java -Dconfig.file="$CONFIG" -jar "$JARFILE" com.socrata.datacoordinator.primary.MigrateSchema "$COMMAND" "$2"
