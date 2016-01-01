7274785ad8ce667508e70575c4f60f613070d934
Robert J. Macomber <robert.macomber@socrata.com>
2015-12-31 16:11:40 -0800

    Bump soql-stdlib dependency
    So that geometry types will be groupable again.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val rojomaJson = "3.2.0"
     val socrataCuratorUtils = "1.0.1"
     val socrataThirdpartyUtils = "4.0.1"
-    val soqlStdlib = "1.0.3"
+    val soqlStdlib = "1.0.4"
     val sprayCaching = "1.2.2"
     val typesafeConfig = "1.0.0"
     val metricsJetty = "3.1.0"
