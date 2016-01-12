872c94daeed597e86cfd0506a85d03b6be06b0e1
Alexa Rust <alexa.rust@socrata.com>
2016-01-12 14:14:11 -0800

    Addresses code review issues up to 2ffd11bf824dab2a3fd239e27dd0bd24acde2b75

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/util/jsoncodecs/package.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/util/jsoncodecs/package.scala
@@ -1,17 +1,8 @@
 package com.socrata.datacoordinator.util
 
-import com.rojoma.json.v3.ast.{JString, JValue}
-import com.rojoma.json.v3.codec.JsonDecode.DecodeResult
-import com.rojoma.json.v3.codec.{DecodeError, JsonDecode, JsonEncode}
+import com.rojoma.json.v3.util.WrapperJsonCodec
 import com.socrata.soql.environment.ColumnName
 
 package object jsoncodecs {
-  implicit object ColumnNameCodec extends JsonEncode[ColumnName] with JsonDecode[ColumnName] {
-    override def encode(x: ColumnName): JValue = JString(x.name)
-
-    override def decode(x: JValue): DecodeResult[ColumnName] = x match {
-      case JString(s) => Right(ColumnName(s))
-      case other => Left(DecodeError.InvalidType(expected = JString, got = other.jsonType))
-    }
-  }
+  implicit val columnNameCodec = WrapperJsonCodec[ColumnName](ColumnName, _.name)
 }
