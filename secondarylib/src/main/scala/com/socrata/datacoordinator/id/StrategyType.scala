3dbb3f2c077a5b245acf311b41981eedfb28abca
Alexa Rust <alexa.rust@socrata.com>
2016-01-12 14:14:11 -0800

    Addresses code review issues up to 2ffd11bf824dab2a3fd239e27dd0bd24acde2b75

--- secondarylib/src/main/scala/com/socrata/datacoordinator/id/StrategyType.scala
+++ secondarylib/src/main/scala/com/socrata/datacoordinator/id/StrategyType.scala
@@ -1,20 +1,11 @@
 package com.socrata.datacoordinator.id
 
-import com.rojoma.json.v3.ast.{JString, JValue}
-import com.rojoma.json.v3.codec.{DecodeError, JsonEncode, JsonDecode}
+import com.rojoma.json.v3.util.WrapperJsonCodec
 
-class StrategyType(val underlying: String) extends AnyVal {
-  override def toString = s"StrategyType($underlying)"
-}
+case class StrategyType(underlying: String) extends AnyVal
 
-object StrategyType {
-  implicit val jCodec = new JsonDecode[StrategyType] with JsonEncode[StrategyType] {
-    def encode(strategyType: StrategyType) = JString(strategyType.underlying)
-    def decode(v: JValue): Either[DecodeError, StrategyType] = v match {
-      case n: JString => Right(new StrategyType(n.string))
-      case other      => Left(DecodeError.InvalidType(JString, other.jsonType))
-    }
-  }
+object StrategyType extends (String => StrategyType) {
+  implicit val jCodec = WrapperJsonCodec[StrategyType](StrategyType, _.underlying)
 
   implicit val ordering = new Ordering[StrategyType] {
     def compare(x: StrategyType, y: StrategyType): Int = Ordering.String.compare(x.underlying, y.underlying)
