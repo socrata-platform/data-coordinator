8e5e60310d82a921e155f710bbf48d88b905c51c
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2016-03-16 23:49:25 -0700

    EN-3976 - Bump soql-stdlib version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val socrataCuratorUtils = "1.0.1"
     val socrataThirdpartyUtils = "4.0.1"
     val socrataUtils = "0.10.0"
-    val soqlStdlib = "2.0.1-SNAPSHOT" // TODO : Use non-snapshot version
+    val soqlStdlib = "2.0.1"
     val sprayCaching = "1.2.2"
     val typesafeConfig = "1.0.0"
     val metricsJetty = "3.1.0"
