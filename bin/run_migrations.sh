008a2b2af2d7b82767a4d6f4a516176904f26af4
Kaida Masaki <kaida.masaki@socrata.com>
2016-04-12 13:21:37 -0700

    PR Feedback

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # Run data coordinator migrations
 # run_migrations.sh [migrate/undo/redo] [numchanges]
 REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
@@ -6,10 +8,11 @@ BASEDIR="$(dirname "${REALPATH}")/.."
 
 CONFIG=${SODA_CONFIG:-/etc/soda2.conf} # TODO: Don't depend on soda2.conf.
 
+cd "$BASEDIR"
 JARFILE="$(ls -rt coordinator/target/scala-*/coordinator-assembly-*.jar 2>/dev/null | tail -n 1)"
 if [ -z "$JARFILE" ] || find ./* -newer "$JARFILE" | egrep -q -v '(/target/)|(/bin/)'; then
-    cd "$BASEDIR" || { echo 'Failed to change directories'; exit; }
     nice -n 19 sbt assembly
+    JARFILE="$(ls -rt coordinator/target/scala-*/coordinator-assembly-*.jar 2>/dev/null | tail -n 1)"
 fi
 
 COMMAND=${1:-migrate}
--- bin/start_watcher.sh
+++ bin/start_watcher.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -e
+
 # Start secondary watcher locally and build it if necessary
 REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BASEDIR="$(dirname "${REALPATH}")/.."
 
 CONFIG=${SODA_CONFIG:-/etc/soda2.conf} # TODO: Don't depend on soda2.conf.
 
+cd "$BASEDIR"
 JARFILE="$(ls -rt coordinator/target/scala-*/coordinator-assembly-*.jar 2>/dev/null | tail -n 1)"
 if [ -z "$JARFILE" ] || find ./* -newer "$JARFILE" | egrep -q -v '(/target/)|(/bin/)'; then
-    cd "$BASEDIR" || { echo 'Failed to change directories'; exit; }
     nice -n 19 sbt assembly
+    JARFILE="$(ls -rt coordinator/target/scala-*/coordinator-assembly-*.jar 2>/dev/null | tail -n 1)"
 fi
 
 java -Djava.net.preferIPv4Stack=true -Dconfig.file="$CONFIG" -jar "$JARFILE" com.socrata.datacoordinator.secondary.SecondaryWatcher
