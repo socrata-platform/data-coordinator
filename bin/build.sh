d8eed030c4b47dc95890d68ed18860c65e288df3
Kaida Masaki <kaida.masaki@socrata.com>
2016-04-14 17:14:05 -0700

    Reorder arguments to `find`

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,7 @@ BASEDIR="$(dirname "${REALPATH}")/.."
 
 cd "$BASEDIR"
 JARFILE="$(ls -rt coordinator/target/scala-*/coordinator-assembly-*.jar 2>/dev/null | tail -n 1)"
-SRC_PATHS=($(find . -name 'src' -o -name '*.sbt' -o -name '*.scala' -maxdepth 2))
+SRC_PATHS=($(find .  -maxdepth 2 -name 'src' -o -name '*.sbt' -o -name '*.scala'))
 if [ -z "$JARFILE" ] || find "${SRC_PATHS[@]}" -newer "$JARFILE" | egrep -q -v '(/target/)|(/bin/)'; then
     if [ "$1" == '--fast' ]; then
         echo 'Assembly is out of date.  Fast start is enabled so skipping rebuild anyway...' >&2
