b151c2e03359492c358628b1efab970d00fcf1a6
Megan Schneider <megan.schneider@socrata.com>
2016-04-28 17:11:27 -0700

    EN-3709: fix datetime row ids in NBE

--- coordinatorlib-soql/src/main/scala/com/socrata/datacoordinator/common/soql/csvreps/FixedTimestampRep.scala
+++ coordinatorlib-soql/src/main/scala/com/socrata/datacoordinator/common/soql/csvreps/FixedTimestampRep.scala
@@ -2,14 +2,27 @@ package com.socrata.datacoordinator.common.soql.csvreps
 
 import com.socrata.datacoordinator.truth.csv.CsvColumnRep
 import com.socrata.soql.types.{SoQLNull, SoQLValue, SoQLFixedTimestamp, SoQLType}
-import org.joda.time.format.DateTimeFormat
+import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormatter, DateTimeFormat}
 
 object FixedTimestampRep extends CsvColumnRep[SoQLType, SoQLValue] {
   val size = 1
 
   val representedType = SoQLFixedTimestamp
 
-  val tsParser = DateTimeFormat.forPattern("MM/dd/yyyy hh:mm aa").withZoneUTC
+  val tsParser = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS").withZoneUTC
+  val alternateTsParser = DateTimeFormat.forPattern("MM/dd/yyyy hh:mm aa").withZoneUTC
+
+  def decoder(formats: DateTimeFormatter*)(raw: String): Option[DateTime] = {
+    formats.foreach { format =>
+      try {
+        return Some(format.parseDateTime(raw))
+      } catch {
+        case e: IllegalArgumentException => // ignore
+      }
+    }
+    None
+  }
 
   def decode(row: IndexedSeq[String], indices: IndexedSeq[Int]): Option[SoQLValue] = {
     assert(indices.size == size)
@@ -17,11 +30,7 @@ object FixedTimestampRep extends CsvColumnRep[SoQLType, SoQLValue] {
     if(x.isEmpty) {
       Some(SoQLNull)
     } else {
-      try {
-        Some(SoQLFixedTimestamp(tsParser.parseDateTime(x)))
-      } catch {
-        case e: IllegalArgumentException => None
-      }
+      decoder(tsParser, alternateTsParser)(x).map(SoQLFixedTimestamp(_))
     }
   }
 }
