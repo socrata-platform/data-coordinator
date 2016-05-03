35b56b5c252f1767aeb6c99aa01790d4b0909ee8
Robert J. Macomber <robert.macomber@socrata.com>
2016-05-03 10:43:30 -0700

    No need to VACUUM ANALYZE right after a copy; just ANALYZE is enough.

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/loader/sql/RepBasedSqlDatasetContentsCopier.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/truth/loader/sql/RepBasedSqlDatasetContentsCopier.scala
@@ -18,7 +18,8 @@ class RepBasedSqlDatasetContentsCopier[CT, CV](conn: Connection, logger: Logger[
         timingReport("copy-dataset-contents", "from-copy" -> from.systemId, "to-copy" -> to.copyInfo.systemId) {
           stmt.execute(s"INSERT INTO ${to.copyInfo.dataTableName} ($physCols) SELECT $physCols FROM ${from.dataTableName}")
         }
-        // TODO: schedule target table for a VACUUM ANALYZE (since it can't happen in a transaction)
+        timingReport("analyze-post-copy", "to-copy" -> to.copyInfo.systemId)
+        stmt.execute(s"ANALYZE ${to.copyInfo.dataTableName}")
       }
       logger.dataCopied()
     }
