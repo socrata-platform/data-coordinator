ed231809dc420add1a15d6c3e8ce5c06c4b968fa
Chi Tang <chi.tang@socrata.com>
2017-02-15 16:40:49 -0800

    Remove logging in DynamicPortMap class.

--- coordinator/src/main/scala/com/socrata/datacoordinator/service/DynamicPortMap.scala
+++ coordinator/src/main/scala/com/socrata/datacoordinator/service/DynamicPortMap.scala
@@ -1,17 +1,12 @@
 package com.socrata.datacoordinator.service
 
-import org.slf4j.Logger
-
 trait DynamicPortMap {
-
-  val log: Logger
 
   private val intRx = "(\\d+)".r
 
   def hostPort(port: Int): Int = {
     Option(System.getenv (s"PORT_$port")) match {
       case Some (intRx (hostPort) ) =>
-        log.info(s"host_port: $hostPort -> container_port: $port")
         hostPort.toInt
       case _ => port
     }
