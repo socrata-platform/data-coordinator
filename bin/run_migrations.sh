990536323ace1fcb22288f97f9d17cba81025eec
Alexa Rust <aerust@uw.edu>
2017-03-30 20:36:20 -0700

    EN-15144: Add column for dataset resource_name (#164)
    We want to start storing dataset resource_name in truth
    so the secondary-watcher can include the external dataset
    ids in messages in AMQ reporting replication complete.
    
    These replication messages are so we can wait to calculate
    column stats after replication is complete.
    
    Also, update migrations script to use sbt instead of first
    assembling a new jar (because that is slow).

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -3,13 +3,9 @@ set -e
 
 # Run data coordinator migrations
 # run_migrations.sh [migrate/undo/redo] [numchanges]
-REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
-BINDIR=$(dirname "$REALPATH")
-
 CONFIG="${SODA_CONFIG:-/etc/soda2.conf}" # TODO: Don't depend on soda2.conf.
-JARFILE=$("$BINDIR"/build.sh "$@")
 
 COMMAND=${1:-migrate}
 echo Running datacoordinator.primary.MigrateSchema "$COMMAND" "$2"...
 ARGS=( $COMMAND $2 )
-java -Djava.net.preferIPv4Stack=true -Dconfig.file="$CONFIG" -jar "$JARFILE" com.socrata.datacoordinator.primary.MigrateSchema "${ARGS[@]}"
+sbt -Dconfig.file="$CONFIG" "coordinator/run-main com.socrata.datacoordinator.primary.MigrateSchema ${ARGS[@]}"
