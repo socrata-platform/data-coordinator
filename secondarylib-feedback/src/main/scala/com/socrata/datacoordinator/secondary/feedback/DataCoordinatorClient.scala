a0577fbf145d2f342ab1fcff15937037d12b4e89
Alexa Rust <aerust@uw.edu>
2017-11-29 12:48:31 -0800

    EN-20087: Stop FeedbackSecondary OOMing on export (#204)
    Fix FeedbackSecondary library code to properly use an iterator
    when exporting row data from data-coordinator to avoid OOMing
    on large datasets.

--- secondarylib-feedback/src/main/scala/com/socrata/datacoordinator/secondary/feedback/DataCoordinatorClient.scala
+++ secondarylib-feedback/src/main/scala/com/socrata/datacoordinator/secondary/feedback/DataCoordinatorClient.scala
@@ -2,27 +2,31 @@ package com.socrata.datacoordinator.secondary.feedback
 
 import java.io.IOException
 
-import com.rojoma.json.v3.ast.{JString, JObject, JValue, JArray}
+import com.rojoma.json.util.JsonArrayIterator.ElementDecodeException
+import com.rojoma.json.v3.ast.{JArray, JObject, JString, JValue}
 import com.rojoma.json.v3.codec.{DecodeError, JsonDecode}
-import com.rojoma.json.v3.io.{JValueEventIterator, JsonReaderException}
-import com.rojoma.json.v3.util.{JsonUtil, NoTag, SimpleHierarchyCodecBuilder, AutomaticJsonCodecBuilder}
+import com.rojoma.json.v3.io.{JValueEventIterator, JsonLexException, JsonReaderException}
+import com.rojoma.json.v3.util._
+import com.rojoma.simplearm.v2.{ResourceScope, using}
 import com.socrata.datacoordinator.id.UserColumnId
 import com.socrata.datacoordinator.secondary
 import com.socrata.datacoordinator.util.collection.MutableColumnIdMap
 import com.socrata.http.client.{HttpClient, RequestBuilder, SimpleHttpRequest}
-import com.socrata.http.client.exceptions.{HttpClientException, ContentTypeException}
+import com.socrata.http.client.exceptions.{ContentTypeException, HttpClientException}
 
 case class RowData[CV](pk: UserColumnId, rows: Iterator[secondary.Row[CV]])
 
 abstract class DataCoordinatorClient[CT, CV](typeFromJValue: JValue => Option[CT],
                                              datasetInternalName: String,
                                              fromJValueFunc: CT => JValue => Option[CV]) {
+
   /**
    * Export the rows of a dataset for the given columns
    * @param columnSet must contain at least on column
    */
   def exportRows(columnSet: Seq[UserColumnId],
-                 cookie: CookieSchema): Either[RequestFailure, Either[ColumnsDoNotExist, RowData[CV]]]
+                 cookie: CookieSchema,
+                 resourceScope: ResourceScope): Either[RequestFailure, Either[ColumnsDoNotExist, RowData[CV]]]
 
   /**
    * Post mutation script to data-coordinator
@@ -69,8 +73,8 @@ class HttpDataCoordinatorClient[CT,CV](httpClient: HttpClient,
     Left(UnexpectedError(message, cause))
   }
 
-  private def unexpectedErrorForResponse(message: String, code: Int, info: Any) =
-    unexpectedError(s"$message from data-coordinator for status $code: $info")
+  private def unexpectedErrorForResponse(message: String, code: Int, info: Any, cause: Exception = null) =
+    unexpectedError(s"$message from data-coordinator for status $code: $info", cause)
 
   private val unexpected = "Received an unexpected error"
   private val uninterpretable = "Unable to interpret error response"
@@ -107,54 +111,52 @@ class HttpDataCoordinatorClient[CT,CV](httpClient: HttpClient,
 
   private def doRequest[T](request: SimpleHttpRequest,
                            message: String,
-                           successHandle: JArray => Either[UnexpectedError, T],
+                           successHandle: Iterator[JValue]=> Either[UnexpectedError, T],
                            badRequestHandle: ErrorResponse => Either[UnexpectedError, T],
-                           serverErrorMessageExtra: String): Either[RequestFailure, T] = {
+                           serverErrorMessageExtra: String,
+                           resourceScope: ResourceScope): Either[RequestFailure, T] = {
     retrying[T] {
-      httpClient.execute(request).run { resp =>
-        val start = System.nanoTime()
-        def logContentTypeFailure(v: => JValue)(f: JValue => Either[RequestFailure, T]): Either[RequestFailure, T] =
-          try {
-            f(v)
-          } catch {
-            case e: ContentTypeException =>
-              log.warn("The response from data-coordinator was not a valid JSON content type! The request we sent was: {}", request.toString)
+      val resp = httpClient.execute(request, resourceScope)
+      val start = System.nanoTime()
+      def logContentTypeFailure[V](v: => JValue)(f: JValue => Either[RequestFailure, T]): Either[RequestFailure, T] =
+        try {
+          f(v)
+        } catch {
+          case e: ContentTypeException =>
+            log.warn("The response from data-coordinator was not a valid JSON content type! The request we sent was: {}", request.toString)
               unexpectedError("Unable to understand data-coordinator's response", e) // no retry here
-          }
-
-        resp.resultCode match {
-          case 200 =>
-            // success! ... well maybe...
-            val end = System.nanoTime()
-            log.info("{} in {}ms", message, (end - start) / 1000000)
-            logContentTypeFailure(resp.jValue()) { JsonDecode.fromJValue[JArray](_) match {
-              case Right(response) => successHandle(response)
-              case Left(e) => unexpectedError(s"Response from data-coordinator was not an array: ${e.english}")
-            }}
-          case 400 =>
-            logContentTypeFailure(resp.jValue()) { JsonDecode.fromJValue[ErrorResponse](_) match {
-              case Right(response) => badRequestHandle(response)
-              case Left(e) => uninterpretableResponse(400, e)
-            }}
-          case 404 =>
-            // { "errorCode" : "update.dataset.does-not-exist"
-            // , "data" : { "dataset" : "XXX.XXX, "data" : { "commandIndex" : 0 }
-            // }
-            logContentTypeFailure(resp.jValue()) { JsonDecode.fromJValue[ErrorResponse](_) match {
-              case Right(ErrorResponse(UpdateDatasetDoesNotExist, _)) => Left(DatasetDoesNotExist)
-              case Right(response) => unexpectedResponse(404, response)
-              case Left(e) => uninterpretableResponse(404, e)
-            }}
-          case 409 =>
-            // come back later!
-            Left(DataCoordinatorBusy) // no retry
-          case other =>
-            if (other == 500) {
-              log.warn("Scream! 500 from data-coordinator! Going to retry; {}", serverErrorMessageExtra)
-            }
-            // force a retry
-            throw new IOException(s"Unexpected result code $other from data-coordinator")
         }
+
+      resp.resultCode match {
+        case 200 =>
+          // success! ... well maybe...
+          val end = System.nanoTime()
+          log.info("{} in {}ms", message, (end - start) / 1000000) // this is kind of a lie... hmm
+          val response = JsonArrayIterator.fromReader[JValue](resp.reader())
+          successHandle(resourceScope.openUnmanaged(response, transitiveClose = List(resp)))
+        case 400 =>
+          logContentTypeFailure(resp.jValue()) { JsonDecode.fromJValue[ErrorResponse](_) match {
+            case Right(response) => badRequestHandle(response)
+            case Left(e) => uninterpretableResponse(400, e)
+          }}
+        case 404 =>
+          // { "errorCode" : "update.dataset.does-not-exist"
+          // , "data" : { "dataset" : "XXX.XXX, "data" : { "commandIndex" : 0 }
+          // }
+          logContentTypeFailure(resp.jValue()) { JsonDecode.fromJValue[ErrorResponse](_) match {
+            case Right(ErrorResponse(UpdateDatasetDoesNotExist, _)) => Left(DatasetDoesNotExist)
+            case Right(response) => unexpectedResponse(404, response)
+            case Left(e) => uninterpretableResponse(404, e)
+          }}
+        case 409 =>
+          // come back later!
+          Left(DataCoordinatorBusy) // no retry
+        case other =>
+          if (other == 500) {
+            log.warn("Scream! 500 from data-coordinator! Going to retry; {}", serverErrorMessageExtra)
+          }
+          // force a retry
+          throw new IOException(s"Unexpected result code $other from data-coordinator")
       }
     }
   }
@@ -169,27 +171,29 @@ class HttpDataCoordinatorClient[CT,CV](httpClient: HttpClient,
    * Export the rows of a dataset for the given columns
    * @param columnSet must contain at least on column
    */
-  def exportRows(columnSet: Seq[UserColumnId], cookie: CookieSchema): Either[RequestFailure, Either[ColumnsDoNotExist, RowData[CV]]] = {
+  def exportRows(columnSet: Seq[UserColumnId],
+                 cookie: CookieSchema,
+                 resourceScope: ResourceScope): Either[RequestFailure, Either[ColumnsDoNotExist, RowData[CV]]] = {
     require(columnSet.nonEmpty, "`columnSet` must be non-empty")
     log.info("Exporting rows for columns: {}", columnSet)
     val endpoint = datasetEndpoint.getOrElse(return Left(FailedToDiscoverDataCoordinator))
     val columns = columnSet.tail.foldLeft(columnSet.head.underlying) { (str, id) => str + "," + id.underlying }
     val builder = RequestBuilder(new java.net.URI(endpoint)).addParameter(("c", columns))
 
-    def successHandle(response: JArray): Either[UnexpectedError, Either[ColumnsDoNotExist, RowData[CV]]] = {
+    def successHandle(response: Iterator[JValue]): Either[UnexpectedError, Either[ColumnsDoNotExist, RowData[CV]]] = {
       if (response.isEmpty) return unexpectedError("Response from data-coordinator was an empty array.")
 
-      JsonDecode.fromJValue[Schema](response.head) match {
+      JsonDecode.fromJValue[Schema](response.next()) match {
         case Right(Schema(pk, schema)) =>
           val columns = schema.map { col =>
             (cookie.columnIdMap(col.c), typeFromJValue(col.t).getOrElse {
               return unexpectedError(s"Could not derive column type for column ${col.c} from value: ${col.t}")
             })
           }.toArray
-          val rows = response.tail.iterator.map {
+          val rows = response.map {
             JsonDecode.fromJValue[JArray](_) match {
               case Right(row) =>
-                assert(row.size == columns.size)
+                assert(row.size == columns.length)
                 val colIdMap = new MutableColumnIdMap[CV]
                 row.iterator.zipWithIndex.foreach { case (jVal, index) =>
                   val (colId, typ) = columns(index)
@@ -201,7 +205,7 @@ class HttpDataCoordinatorClient[CT,CV](httpClient: HttpClient,
               case Left(e) => return unexpectedErrorForResponse("Row data was not an array", 200, e.english)
             }
           }
-          Right(Right(RowData(pk, rows)))
+          Right(Right(resourceScope.openUnmanaged(RowData(pk, rows), transitiveClose = List(response))))
         case Left(e) => unexpectedErrorForResponse("Response did not start with expected column schema", 200, e.english)
       }
     }
@@ -220,7 +224,8 @@ class HttpDataCoordinatorClient[CT,CV](httpClient: HttpClient,
       message = s"Got row data for columns $columns",
       successHandle,
       badRequestHandle,
-      serverErrorMessageExtra = s"my parameters where: c=$columns"
+      serverErrorMessageExtra = s"my parameters where: c=$columns",
+      resourceScope
     )
   }
 
@@ -238,23 +243,29 @@ class HttpDataCoordinatorClient[CT,CV](httpClient: HttpClient,
 
     def body = JValueEventIterator(script)
 
-    def successHandle(response: JArray): Either[UnexpectedError, Option[UpdateSchemaFailure]] = {
-      assert(response.elems.length == 1, "Response contains more than one element")
-      JsonDecode.fromJValue[JArray](response.elems.head) match {
-        case Right(results) =>
-          assert(results.elems.length == script.elems.length - 2, "Did not get one result for each upsert command that was sent")
-          val rows = script.elems.slice(2, script.elems.length)
-          results.elems.zip(rows).foreach { case (result, row) =>
-            JsonDecode.fromJValue[Response](result) match {
-              case Right(Upsert("update", _, _)) => // yay!
-              case Right(NonfatalError("error", nonfatalError, Some(_))) if nonfatalError == "insert_in_update_only" ||  nonfatalError == "no_such_row_to_update" =>
+    def successHandle(response: Iterator[JValue]): Either[UnexpectedError, Option[UpdateSchemaFailure]] = {
+      try {
+        assert(response.hasNext, "Response contains no elements")
+        JsonDecode.fromJValue[JArray](response.next()) match {
+          case Right(results) =>
+            assert(!response.hasNext, "Response contains more than one element")
+            assert(results.elems.length == script.elems.length - 2, "Did not get one result for each upsert command that was sent")
+            val rows = script.elems.slice(2, script.elems.length)
+            results.elems.zip(rows).foreach { case (result, row) =>
+              JsonDecode.fromJValue[Response](result) match {
+                case Right(Upsert("update", _, _)) => // yay!
+                case Right(NonfatalError("error", nonfatalError, Some(_))) if nonfatalError == "insert_in_update_only" || nonfatalError == "no_such_row_to_update" =>
                 // the row has been deleted, nothing more to do here
-              case Right(other) => unexpectedErrorForResponse("Unexpected response in array", 200, other)
-              case Left(e) => unexpectedErrorForResponse("Unable to interpret result in array", 200, e.english)
+                case Right(other) => unexpectedErrorForResponse("Unexpected response in array", 200, other)
+                case Left(e) => unexpectedErrorForResponse("Unable to interpret result in array", 200, e.english)
+              }
             }
-          }
-          Right(None)
-        case Left(e) => unexpectedErrorForResponse("Row data was not an array", 200, e.english)
+            Right(None)
+          case Left(e) => unexpectedErrorForResponse("Row data was not an array", 200, e.english)
+        }
+      } catch {
+        case e: JsonLexException => unexpectedErrorForResponse("Unable to parse response as JSON", 200, e.message, e)
+        case e: ElementDecodeException => unexpectedErrorForResponse("Unable to parse an element of the response as JSON", 200, e.position, e)
       }
     }
 
@@ -272,16 +283,21 @@ class HttpDataCoordinatorClient[CT,CV](httpClient: HttpClient,
       }
     }
 
-    doRequest(
-      request = builder.json(body),
-      message = s"Posted mutation script with ${script.length - 2} row updates",
-      successHandle,
-      badRequestHandle,
-      serverErrorMessageExtra = s"my mutation script was: ${JsonUtil.renderJson(script, pretty = false)}"
-    ) match {
-      case Left(failure) => Some(Left(failure))
-      case Right(Some(failure)) => Some(Right(failure))
-      case Right(None) => None
+    val requestResult = using(new ResourceScope("post mutation script")) { resourceScope =>
+      doRequest(
+        request = builder.json(body),
+        message = s"Posted mutation script with ${script.length - 2} row updates",
+        successHandle,
+        badRequestHandle,
+        serverErrorMessageExtra = s"my mutation script was: ${JsonUtil.renderJson(script, pretty = false)}",
+        resourceScope
+      ) match {
+        case Left(failure) => Some(Left(failure))
+        case Right(Some(failure)) => Some(Right(failure))
+        case Right(None) => None
+      }
     }
+
+    requestResult
   }
 }
