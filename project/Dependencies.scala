53be6f8218df06f747ee579804a5f60689194b2e
Carlson <eddie.carlson@seappltecarlson.local>
2018-10-18 15:35:56 -0700

    versioning

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val scalaMock       = "3.2"
     val scalaTest       = "2.2.4"
     val slf4j           = "1.7.5"
-    val soqlReference   = "2.9.0"
+    val soqlReference   = "2.10.6"
     val thirdPartyUtils = "4.0.1"
     val curatorUtils    = "1.0.3"
     val httpCuratorBroker = "3.10.1"
