9d5ab5cf0639975229430cd42ad77f013e968f55
Robert J. Macomber <robert.macomber@socrata.com>
2019-12-09 14:53:26 -0800

    Update the one script in `bin` that had a hardcoded scala version

--- bin/build_dc_docker.sh
+++ bin/build_dc_docker.sh
@@ -3,6 +3,6 @@
 set -e
 
 sbt coordinator/assembly
-JARFILE="$(ls -t coordinator/target/scala-2.10/coordinator-assembly*.jar | head -1)"
+JARFILE="$(ls -t coordinator/target/scala-2.12/coordinator-assembly*.jar | head -1)"
 cp "$JARFILE" "coordinator/docker/coordinator-assembly.jar"
 docker build --pull=true -t data-coordinator coordinator/docker
