ee7ba8d602452108655c135c2ac6fa02c5948259
rozap <chrisd1891@gmail.com>
2021-02-25 18:24:49 -0800

    SoQLJson WIP

---
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/common/soql/sqlreps/JsonRep.scala
@@ -0,0 +1,62 @@
+package com.socrata.datacoordinator.common.soql.sqlreps
+
+import java.lang.StringBuilder
+import java.sql.{PreparedStatement, ResultSet, Types}
+
+import com.rojoma.json.v3.ast.{JNull, JValue}
+import com.rojoma.json.v3.util.JsonUtil
+import com.socrata.datacoordinator.truth.sql.SqlColumnRep
+import com.socrata.soql.types._
+
+class JsonRep(val base: String) extends RepUtils with SqlColumnRep[SoQLType, SoQLValue] {
+  def representedType: SoQLType = SoQLJson
+
+  val physColumns: Array[String] = Array(base)
+
+  val sqlTypes: Array[String] = Array("JSONB")
+
+  override def templateForUpdate: String = physColumns.map(_ + "=?::JSONB").mkString(",")
+
+  override def templateForInsert: String = physColumns.map(_ => "?::JSONB").mkString(",")
+
+  def csvifyForInsert(sb: StringBuilder, v: SoQLValue): Unit = {
+    v match {
+      case SoQLJson(x) =>
+        csvescape(sb, JsonUtil.renderJson(x))
+      case SoQLNull =>
+      case unknown =>
+        throw new Exception("unknown SoQLValue")
+    }
+  }
+
+  def prepareInsert(stmt: PreparedStatement, v: SoQLValue, start: Int): Int = {
+    v match {
+      case SoQLJson(x) =>
+        stmt.setObject(start, JsonUtil.renderJson(x))
+      case SoQLNull =>
+        stmt.setNull(start, Types.VARCHAR)
+      case unknown =>
+        throw new Exception("unknown SoQLValue")
+    }
+    start + 1
+  }
+
+  def estimateSize(v: SoQLValue): Int = {
+    v match {
+      case SoQLNull => standardNullInsertSize
+      case SoQLJson(js) => JsonUtil.renderJson(js).size
+      case _ => ???
+    }
+  }
+
+  def fromResultSet(rs: ResultSet, start: Int): SoQLValue = {
+    Option(rs.getString(start)) match {
+      case None => SoQLJson(JNull)
+      case Some(js) =>
+        JsonUtil.parseJson[JValue](js) match {
+          case Right(jv) => SoQLJson(jv)
+          case _ => SoQLNull // not sure about this.
+        }
+    }
+  }
+}
