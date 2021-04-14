07ac79cf7c39a26d4087ccca4a64ccebbcb76009
rozap <chrisd1891@gmail.com>
2021-04-14 16:42:21 -0700

    disambigute nulls

--- coordinatorlib/src/main/scala/com/socrata/datacoordinator/common/soql/jsonreps/JsonRep.scala
+++ coordinatorlib/src/main/scala/com/socrata/datacoordinator/common/soql/jsonreps/JsonRep.scala
@@ -1,6 +1,6 @@
 package com.socrata.datacoordinator.common.soql.jsonreps
 
-import com.rojoma.json.v3.ast.{JNull, JValue}
+import com.rojoma.json.v3.ast.{JNull, JValue, JObject}
 import com.rojoma.json.v3.codec.JsonDecode
 import com.socrata.datacoordinator.truth.json.JsonColumnRep
 import com.socrata.soql.types._
@@ -10,13 +10,16 @@ object JsonRep extends JsonColumnRep[SoQLType, SoQLValue] {
   val representedType = SoQLJson
 
   def fromJValue(input: JValue): Option[SoQLValue] = {
-    Some(SoQLJson(input))
+    input match {
+      case JObject(m) => m.get("json").map(js => SoQLJson(js))
+      case _ => None
+    }
   }
 
   def toJValue(input: SoQLValue): JValue = {
     input match {
-      case x@SoQLJson(value) => value
-      case SoQLNull => JNull // TODO: this needs to be disambiguated from SoQLNull? is it?
+      case x@SoQLJson(value) => JObject(Map("json" -> value))
+      case SoQLNull => JNull
       case _ => stdBadValue
     }
   }
