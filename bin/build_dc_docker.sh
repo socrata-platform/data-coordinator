6b06d53d52e6f72fb875b3be337b5ce210b64198
Robert J. Macomber <robert.macomber@socrata.com>
2023-08-22 10:19:04 -0700

    Update bin scripts to reflect the new simplified assembly naming

--- bin/build_dc_docker.sh
+++ bin/build_dc_docker.sh
@@ -3,6 +3,6 @@
 set -e
 
 sbt coordinator/assembly
-JARFILE="$(ls -t coordinator/target/scala-2.12/coordinator-assembly*.jar | head -1)"
+JARFILE="coordinator/target/coordinator-assembly.jar"
 cp "$JARFILE" "coordinator/docker/coordinator-assembly.jar"
 docker build --pull=true -t data-coordinator coordinator/docker
