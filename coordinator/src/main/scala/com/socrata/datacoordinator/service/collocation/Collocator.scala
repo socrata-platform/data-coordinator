3b741294f1532e65a81b056f6e7feca183614893
Robert J. Macomber <robert.macomber@socrata.com>
2024-10-21 11:02:26 -0700

    Revert "Revert "Build dataset group map more efficiently""
    This reverts commit 993ed68358c239645f84fd2016a37b5f750a9c30.

--- coordinator/src/main/scala/com/socrata/datacoordinator/service/collocation/Collocator.scala
+++ coordinator/src/main/scala/com/socrata/datacoordinator/service/collocation/Collocator.scala
@@ -167,7 +167,7 @@ class CoordinatedCollocator(collocationGroup: Set[String],
                 Set(collocation._1, collocation._2)
               }.toSet
 
-              val inputDatasets = collocationEdges.flatten // example: Set(alpha.1, alpha.2)
+              val inputDatasets: Set[DatasetInternalName] = collocationEdges.flatten // example: Set(alpha.1, alpha.2)
 
               // we will 404 if we get a 404 projected stores for one of the input datasets
               // example: Set(alpha.1 -> Set(pg1, pg2), alpha.2 -> Set(pg3, pg4))
@@ -183,11 +183,29 @@ class CoordinatedCollocator(collocationGroup: Set[String],
               }.toMap // datasetStoreMap: key = dataset, values = set of secondary_instances the dataset lives
               log.info("Dataset stores map: {}", JsonEncode.toJValue(datasetStoresMap))
 
-              val datasetGroupMap: Map[DatasetInternalName, Set[DatasetInternalName]] = inputDatasets.map { dataset =>
-                val collocateDatasetsResult: Set[DatasetInternalName] =
-                  logTime(s"collocatedDatasets $dataset")(collocatedDatasets(Set(dataset)).fold(throw _, _.datasets))
-                (dataset, collocateDatasetsResult)
-              }.toMap // datasetGroupMap: key = dataset, value = set of all related datasets in the collocation_manifest table.
+              val datasetGroupMap: Map[DatasetInternalName, Set[DatasetInternalName]] = inputDatasets.foldLeft(Map.empty[DatasetInternalName, Set[DatasetInternalName]]) { (acc, dataset) =>
+                // if a dataset's colloc group is already known due to
+                // being collocated with an earlier group, we don't
+                // have do actually find the collocation group now.
+                // So what we're doing here is saying "for each input
+                // dataset, if we haven't already found it in a
+                // collocation group, look up its colloc group and
+                // associate it with every dataset in that colloc
+                // group".  In the best case, this means we're only
+                // doing the lookup call once.
+                acc.get(dataset) match {
+                  case Some(_) =>
+                    acc
+                  case None =>
+                    val collocateDatasetsResult: Set[DatasetInternalName] =
+                      logTime(s"collocatedDatasets $dataset")(collocatedDatasets(Set(dataset)).fold(throw _, _.datasets))
+
+                    acc ++ inputDatasets.iterator.flatMap { input =>
+                      if(collocateDatasetsResult(input)) Some(input -> collocateDatasetsResult)
+                      else None
+                    }
+                }
+              } // datasetGroupMap: key = dataset, value = set of all related datasets in the collocation_manifest table.
               log.info("Dataset group map: {}", JsonEncode.toJValue(datasetGroupMap))
 
               // cost of _all_ datasets in question, not just the input datasets
