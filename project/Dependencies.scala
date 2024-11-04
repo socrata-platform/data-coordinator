3e6407b3f136fa3e66e13a4dc2d4b70cd637f6d2
Peter Moore <peter.moore@socrata.com>
2024-11-04 16:55:58 -0500

    bump activemq dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object versions {
-    val activemq        = "5.15.9"
+    val activemq        = "5.15.16"
     val c3po            = "0.9.5-pre9"
     val clojure         = "1.5.1"
     val commonsCodec    = "1.8"
